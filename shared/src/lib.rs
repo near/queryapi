46d78b35e8f2b1055b4b02a2f16a7585c05beefb
Bohdan Khorolets <bogdan@khorolets.com>
2022-07-26 16:19:39 +0300

    refactor(shared): Propagate message id in SQS batch sender (required by SQS)

--- shared/src/lib.rs
+++ shared/src/lib.rs
@@ -214,12 +214,14 @@ pub async fn send_to_the_queue(
     let message_bodies: Vec<SendMessageBatchRequestEntry> = alert_queue_messages
         .into_iter()
         .map(|alert_queue_message| {
+            let message = base64::encode(
+                alert_queue_message
+                    .try_to_vec()
+                    .expect("Failed to BorshSerialize AlertQueueMessage"),
+            );
             SendMessageBatchRequestEntry::builder()
-                .message_body(base64::encode(
-                    alert_queue_message
-                        .try_to_vec()
-                        .expect("Failed to BorshSerialize AlertQueueMessage"),
-                ))
+                .id(&message) // The Ids of a batch request need to be unique within a request. Up to 80 chars
+                .message_body(message)
                 .build()
         })
         .collect();
