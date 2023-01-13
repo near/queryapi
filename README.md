2328b24ac51a9e44a51530c9f4f707b1cf708557
Gabe Hamilton <gabehamilton@gmail.com>
2023-01-13 00:55:28 -0700

    chore: Tweaks and instructions to run locally. (#29)
    * Tweaks and instructions to run locally.
    
    * Update alert-rules/src/lib.rs
    
    Co-authored-by: Bohdan Khorolets <b@khorolets.com>
    
    * Copied latest DB schema from console for standalone Alertexer use.
    
    Co-authored-by: Bohdan Khorolets <b@khorolets.com>

--- README.md
+++ README.md
@@ -23,7 +23,7 @@ This project is using `workspace` feature of Cargo.
 
 Closely related to the project but excluded from the workspace for different reasons crates.
 
-- [`queue-handler`](./queue-handler) is an AWS lambda function (Rust-lang) that listens to the events in main AWS SQS queue for alertexer. Interacts with the DevConsole DB to get data about the `AlertRule` and stores info about triggered events, passed the triggered event to the relevant queue based on the delivery channel.
+- [`queue-handler-alertexer`](./queue-handler-alertexer) is an AWS lambda function (Rust-lang) that listens to the events in main AWS SQS queue for alertexer. Interacts with the DevConsole DB to get data about the `AlertRule` and stores info about triggered events, passed the triggered event to the relevant queue based on the delivery channel.
 - [`webhook-queue-handler`](./webhook-queue-handler) is an AWS lambda function (Rust-lang) that listens to the events in the dedicated AWS SQS queue for webhooks. Interacts with the DB to store an information about the webhook is sent and what was the response (in order to simplify the webhook debugging)
 - [`telegram-queue-handler`](./telegram-queue-handler) is an AWS lambda function (Rust-lang) that listens to the events in the dedicated AWS SQS queue for Telegram. Interacts with the DevConsole DB to get the `AlertRule`'s name in order to create a Telegram message
   > "Alert {name} triggered. See {link to NEAR Explorer} for details"
@@ -48,3 +48,22 @@ QUEUE_AWS_SECRET_ACCESS_KEY=SQS_ACCESS_SECRET
 QUEUE_URL=https://sqs.eu-central-1.amazonaws.com/754641474505/alertexer-queue
 
 ```
+## Running locally
+ * _Install postgres locally if not already present._
+ * Create a local postgres database and user like so, changing the credentials to your liking:
+```
+psql 
+CREATE DATABASE alerts;
+CREATE USER alerts WITH PASSWORD 'alerts';
+GRANT ALL PRIVILEGES ON DATABASE alerts TO alerts;
+```
+ * Update the `.env` file with the database credentials you just set. `host.docker.internal` as the hostname will point to your local host machine. 
+ * Run [schema.sql](./alert-rules/schema.sql) against your alerts DB to create the alert rules tables.
+ * Grant table privileges to the DB user
+```
+psql
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO alerts;
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO alerts;
+```
+ * _Install docker locally if not already present._
+ * Run `docker compose up`
--- alert-rules/schema.sql
+++ alert-rules/schema.sql
@@ -1,68 +1,181 @@
+-- This schema can be used when running standalone, however this DB is shared with Pagoda Console.
+-- The Console schema is the controlling schema, See https://github.com/near/pagoda-console/blob/develop/database/schemas/alerts/schema.prisma
+--   and the migrations generated from that file.
+
 CREATE TYPE alert_rule_kind AS ENUM(
     'ACTIONS', -- RECEIPTS Action (receipts)
-    'EVENTS' --- Event(execution outcome logs)
+    'EVENTS', --- Event(execution outcome logs)
+    'STATE_CHANGES'
     --- ACCOUNT_BALANCES state change (accounts)
 );
 
-CREATE TYPE chain_id AS ENUM (
-    'MAINNET',
-    'TESTNET'
+CREATE TYPE chain_id AS ENUM (  'MAINNET', 'TESTNET');
+
+CREATE TYPE destination_type AS ENUM ('WEBHOOK', 'EMAIL', 'TELEGRAM');
+
+CREATE TABLE "alert_rules" (
+    "id" SERIAL NOT NULL,
+    "alert_rule_kind" "alert_rule_kind" NOT NULL,
+    "name" TEXT NOT NULL,
+    "matching_rule" JSONB NOT NULL,
+    "is_paused" BOOLEAN NOT NULL DEFAULT false,
+    "project_slug" TEXT NOT NULL,
+    "environment_sub_id" INTEGER NOT NULL,
+    "chain_id" "chain_id" NOT NULL,
+    "active" BOOLEAN NOT NULL DEFAULT true,
+    "created_at" TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "created_by" INTEGER NOT NULL,
+    "updated_at" TIMESTAMPTZ,
+    "updated_by" INTEGER,
+
+    CONSTRAINT "alert_rules_pkey" PRIMARY KEY ("id")
 );
 
-CREATE TYPE destination_type AS ENUM(
-    'WEBHOOK'
-    --- 'SLACK',
-    --- 'TELEGRAM',
-    --- 'EMAIL',
+CREATE TABLE "destinations" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT,
+    "project_slug" TEXT NOT NULL,
+    "type" "destination_type" NOT NULL,
+    "active" BOOLEAN NOT NULL DEFAULT true,
+    "is_valid" BOOLEAN NOT NULL DEFAULT false,
+    "created_at" TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    "created_by" INTEGER,
+    "updated_at" TIMESTAMPTZ,
+    "updated_by" INTEGER,
+
+    CONSTRAINT "destinations_pkey" PRIMARY KEY ("id")
 );
 
-CREATE TABLE alert_rules (
-    id serial PRIMARY KEY,
-    name text NOT NULL,
-    chain_id chain_id NOT NULL,
-    alert_rule_kind alert_rule_kind NOT NULL,
-    matching_rule json NOT NULL,
-    is_paused bool NOT NULL,
-    updated_at timestamptz NOT NULL
+CREATE TABLE "enabled_destinations" (
+    "id" SERIAL NOT NULL,
+    "alert_id" INTEGER NOT NULL,
+    "destination_id" INTEGER NOT NULL,
+    "created_at" TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    "created_by" INTEGER,
+    "updated_at" TIMESTAMPTZ,
+    "updated_by" INTEGER,
+
+    CONSTRAINT "enabled_destinations_pkey" PRIMARY KEY ("id")
 );
 
-CREATE TABLE destinations (
-    id serial PRIMARY KEY,
-    name text NOT NULL,
-    active bool NOT NULL DEFAULT true,
-    type notification_channel_kind NOT NULL
+CREATE TABLE "webhook_destinations" (
+    "id" SERIAL NOT NULL,
+    "destination_id" INTEGER NOT NULL,
+    "url" TEXT NOT NULL,
+    "secret" TEXT NOT NULL,
+    "created_at" TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    "created_by" INTEGER,
+    "updated_at" TIMESTAMPTZ,
+    "updated_by" INTEGER,
+
+    CONSTRAINT "webhook_destinations_pkey" PRIMARY KEY ("id")
 );
 
-CREATE TABLE enabled_destinations (
-    id serial PRIMARY KEY,
-    alert_id int NOT NULL,
-    destination_id int NOT NULL,
-    FOREIGN KEY (destination_id) REFERENCES destinations (id),
-    FOREIGN KEY (alert_id) REFERENCES alert_rules (id)
+-- CreateTable
+CREATE TABLE "email_destinations" (
+    "id" SERIAL NOT NULL,
+    "destination_id" INTEGER NOT NULL,
+    "email" TEXT NOT NULL,
+    "token" TEXT,
+    "is_verified" BOOLEAN NOT NULL DEFAULT false,
+    "token_expires_at" TIMESTAMPTZ,
+    "unsubscribe_token" TEXT,
+    "created_at" TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    "created_by" INTEGER,
+    "updated_at" TIMESTAMPTZ,
+    "updated_by" INTEGER,
+    "token_created_at" TIMESTAMPTZ,
+    CONSTRAINT "email_destinations_pkey" PRIMARY KEY ("id")
 );
 
-CREATE TABLE triggered_alerts (
-    id serial PRIMARY KEY,
-    alert_id int NOT NULL,
-    triggered_in_block_hash text NOT NULL,
-    triggered_in_transaction_hash text,
-    triggered_in_receipt_id text,
-    triggered_at timestamptz NOT NULL,
-    FOREIGN KEY (alert_id) REFERENCES alert_rules (id)
+-- CreateTable
+CREATE TABLE "telegram_destinations" (
+    "id" SERIAL NOT NULL,
+    "destination_id" INTEGER NOT NULL,
+    "chat_id" DOUBLE PRECISION,
+    "chat_title" TEXT,
+    "is_group_chat" BOOLEAN,
+    "start_token" TEXT,
+    "token_expires_at" TIMESTAMPTZ,
+    "created_at" TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "created_by" INTEGER,
+    "updated_at" TIMESTAMPTZ,
+    "updated_by" INTEGER,
+
+    CONSTRAINT "telegram_destinations_pkey" PRIMARY KEY ("id")
 );
 
-CREATE TABLE triggered_alerts_destinations (
-    triggered_alert_id int NOT NULL,
-    alert_id int NOT NULL,
-    destination_id int NOT NULL,
-    status int NOT NULL,
-    response text NOT NULL,
-    created_at timestamptz NOT NULL,
-    FOREIGN KEY (triggered_alert_id) REFERENCES triggered_alerts (id),
-    FOREIGN KEY (alert_id) REFERENCES alert_rules (id),
-    FOREIGN KEY (destination_id) REFERENCES destinations (id)
+-- CreateTable
+CREATE TABLE "triggered_alerts" (
+    "id" SERIAL NOT NULL,
+    "slug" TEXT NOT NULL DEFAULT gen_random_uuid(),
+    "alert_id" INTEGER NOT NULL,
+    "triggered_in_block_hash" TEXT NOT NULL,
+    "triggered_in_transaction_hash" TEXT,
+    "triggered_in_receipt_id" TEXT,
+    "triggered_at" TIMESTAMPTZ NOT NULL,
+    "extra_data" JSONB,
+
+    CONSTRAINT "triggered_alerts_pkey" PRIMARY KEY ("id")
 );
 
-ALTER TABLE triggered_alerts_destinations
-    ADD CONSTRAINT triggered_alerts_destinations_pk PRIMARY KEY (triggered_alert_id, alert_id, destination_id);
+-- CreateTable
+CREATE TABLE "triggered_alerts_destinations" (
+    "triggered_alert_id" INTEGER NOT NULL,
+    "alert_id" INTEGER NOT NULL,
+    "destination_id" INTEGER NOT NULL,
+    "status" INTEGER NOT NULL,
+    "response" TEXT NOT NULL,
+    "created_at" TIMESTAMPTZ NOT NULL,
+
+    CONSTRAINT "triggered_alerts_destinations_pkey" PRIMARY KEY ("triggered_alert_id","alert_id","destination_id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "enabled_destinations_destination_id_alert_id_key" ON "enabled_destinations"("destination_id", "alert_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "webhook_destinations_destination_id_key" ON "webhook_destinations"("destination_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "email_destinations_destination_id_key" ON "email_destinations"("destination_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "email_destinations_token_key" ON "email_destinations"("token");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "email_destinations_unsubscribe_token_key" ON "email_destinations"("unsubscribe_token");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "telegram_destinations_destination_id_key" ON "telegram_destinations"("destination_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "telegram_destinations_start_token_key" ON "telegram_destinations"("start_token");
+
+-- AddForeignKey
+ALTER TABLE "enabled_destinations" ADD CONSTRAINT "enabled_destinations_alert_id_fkey" FOREIGN KEY ("alert_id") REFERENCES "alert_rules"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "enabled_destinations" ADD CONSTRAINT "enabled_destinations_destination_id_fkey" FOREIGN KEY ("destination_id") REFERENCES "destinations"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "webhook_destinations" ADD CONSTRAINT "webhook_destinations_destination_id_fkey" FOREIGN KEY ("destination_id") REFERENCES "destinations"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "email_destinations" ADD CONSTRAINT "email_destinations_destination_id_fkey" FOREIGN KEY ("destination_id") REFERENCES "destinations"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "telegram_destinations" ADD CONSTRAINT "telegram_destinations_destination_id_fkey" FOREIGN KEY ("destination_id") REFERENCES "destinations"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "triggered_alerts" ADD CONSTRAINT "triggered_alerts_alert_id_fkey" FOREIGN KEY ("alert_id") REFERENCES "alert_rules"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "triggered_alerts_destinations" ADD CONSTRAINT "triggered_alerts_destinations_alert_id_fkey" FOREIGN KEY ("alert_id") REFERENCES "alert_rules"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "triggered_alerts_destinations" ADD CONSTRAINT "triggered_alerts_destinations_destination_id_fkey" FOREIGN KEY ("destination_id") REFERENCES "destinations"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "triggered_alerts_destinations" ADD CONSTRAINT "triggered_alerts_destinations_triggered_alert_id_fkey" FOREIGN KEY ("triggered_alert_id") REFERENCES "triggered_alerts"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
