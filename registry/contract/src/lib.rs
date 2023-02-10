23188699ce673b6015fefe08fe97750dc1b6bc4a
Roshaan Siddiqui <siddiqui.roshaan@gmail.com>
2023-02-10 11:56:53 -0600

    feat: added schema + start_block_height params

--- registry/contract/src/lib.rs
+++ registry/contract/src/lib.rs
@@ -1,6 +1,7 @@
 // Find all our documentation at https://docs.near.org
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::{near_bindgen, env, log};
+use near_sdk::serde::{Deserialize, Serialize};
+use near_sdk::{env, log, near_bindgen};
 
 use std::collections::HashMap;
 
@@ -8,13 +9,24 @@ use std::collections::HashMap;
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize)]
 pub struct Contract {
-    registry: HashMap<String, String>,
+    registry: HashMap<String, IndexerConfig>,
+}
+
+// Define the contract structure
+#[derive(BorshDeserialize, BorshSerialize, Serialize, Deserialize, Clone, Debug, PartialEq, Eq)]
+#[serde(crate = "near_sdk::serde")]
+pub struct IndexerConfig {
+    code: String,
+    start_block_height: Option<u64>,
+    schema: Option<String>,
 }
 
 // Define the default, which automatically initializes the contract
-impl Default for Contract{
-    fn default() -> Self{
-        Self{registry: HashMap::new()}
+impl Default for Contract {
+    fn default() -> Self {
+        Self {
+            registry: HashMap::new(),
+        }
     }
 }
 
@@ -22,26 +34,50 @@ impl Default for Contract{
 #[near_bindgen]
 impl Contract {
     // Public method - returns a function previously registered under this name or empty string
-    pub fn read_indexer_function(&self, name: String) -> String {
-        return self.registry.get(&name).unwrap_or(&"".to_string()).to_string();
+    pub fn read_indexer_function(&self, name: String) -> IndexerConfig {
+        match self.registry.get(&name) {
+            Some(config) => config.clone(),
+            None => IndexerConfig {
+                code: "".to_string(),
+                start_block_height: None,
+                schema: None,
+            },
+        }
     }
 
     // Public method - registers indexer code under <account_id>/name
-    pub fn register_indexer_function(&mut self, name: String, code: String) {
+    pub fn register_indexer_function(
+        &mut self,
+        name: String,
+        code: String,
+        start_block_height: Option<u64>,
+        schema: Option<String>,
+    ) {
         let signer_account_id = env::signer_account_id().as_str().to_string();
         let registered_name = [signer_account_id, name].join("/");
-        log!("Registering function with account and name {}", &registered_name);
-        self.registry.insert(registered_name, code);
+        let config = IndexerConfig {
+            code,
+            start_block_height,
+            schema,
+        };
+        log!(
+            "Registering function with account and name {}",
+            &registered_name
+        );
+        self.registry.insert(registered_name, config);
     }
 
     pub fn remove_indexer_function(&mut self, name: String) {
         let signer_account_id = env::signer_account_id().as_str().to_string();
         let registered_name = [signer_account_id, name].join("/");
-        log!("Removing function with account and name {}", &registered_name);
+        log!(
+            "Removing function with account and name {}",
+            &registered_name
+        );
         self.registry.remove(&registered_name);
     }
 
-    pub fn list_indexer_functions(&self) -> HashMap<String, String> {
+    pub fn list_indexer_functions(&self) -> HashMap<String, IndexerConfig> {
         self.registry.clone()
     }
 }
@@ -57,44 +93,86 @@ mod tests {
     #[test]
     fn get_empty() {
         let contract = Contract::default();
+        let empty_config = IndexerConfig {
+            code: "".to_string(),
+            start_block_height: None,
+            schema: None,
+        };
         // no registered indexers so should return the default ""
         assert_eq!(
             contract.read_indexer_function("developer.near/test".to_string()),
-            "".to_string()
+            empty_config
         );
     }
 
     #[test]
     fn set_then_get_indexer_function() {
         let mut contract = Contract::default();
-        contract.register_indexer_function("test".to_string(), "var x= 1;".to_string());
-        assert_eq!( // default account is bob.near
+        let config = IndexerConfig {
+            code: "var x= 1;".to_string(),
+            start_block_height: Some(43434343),
+            schema: None,
+        };
+        contract.register_indexer_function(
+            "test".to_string(),
+            config.code.clone(),
+            config.start_block_height.clone(),
+            config.schema.clone(),
+        );
+        assert_eq!(
+            // default account is bob.near
             contract.read_indexer_function("bob.near/test".to_string()),
-            "var x= 1;".to_string()
+            config
         );
     }
 
     #[test]
     fn set_then_get_then_remove_indexer_function() {
         let mut contract = Contract::default();
-        contract.register_indexer_function("test".to_string(), "var x= 1;".to_string());
-        assert_eq!( // default account is bob.near
-                    contract.read_indexer_function("bob.near/test".to_string()),
-                    "var x= 1;".to_string()
+        let config = IndexerConfig {
+            code: "var x= 1;".to_string(),
+            start_block_height: None,
+            schema: None,
+        };
+        contract.register_indexer_function(
+            "test".to_string(),
+            config.code.clone(),
+            config.start_block_height.clone(),
+            config.schema.clone(),
+        );
+        assert_eq!(
+            // default account is bob.near
+            contract.read_indexer_function("bob.near/test".to_string()),
+            config
         );
         contract.remove_indexer_function("test".to_string());
+        let empty_config = IndexerConfig {
+            code: "".to_string(),
+            start_block_height: None,
+            schema: None,
+        };
         assert_eq!(
-                    contract.read_indexer_function("bob.near/test".to_string()),
-                    "".to_string()
+            contract.read_indexer_function("bob.near/test".to_string()),
+            empty_config
         );
     }
 
     #[test]
     fn set_then_list_indexer_functions() {
         let mut contract = Contract::default();
-        contract.register_indexer_function("test".to_string(), "var x= 1;".to_string());
+        let config = IndexerConfig {
+            code: "var x= 1;".to_string(),
+            start_block_height: Some(4343333),
+            schema: Some("key: string, value: string".to_string()),
+        };
+        contract.register_indexer_function(
+            "test".to_string(),
+            config.code.clone(),
+            config.start_block_height.clone(),
+            config.schema.clone(),
+        );
         let mut expected = HashMap::new();
-        expected.insert("bob.near/test".to_string(), "var x= 1;".to_string());
+        expected.insert("bob.near/test".to_string(), config);
         assert_eq!(contract.list_indexer_functions(), expected);
     }
 }
