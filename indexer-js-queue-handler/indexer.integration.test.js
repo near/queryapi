502d458e023586ab8b42b9e6e67c4b693c0a81e2
Gabe Hamilton <gabe@near.org>
2023-03-01 16:26:07 -0700

    Logs are written to indexer_log_entries table. IndexerFunctions can console.log Block height written to indexer_state table

--- indexer-js-queue-handler/indexer.integration.test.js
+++ indexer-js-queue-handler/indexer.integration.test.js
@@ -25,17 +25,17 @@ describe('Indexer integration tests', () => {
         functions['buildnear.testnet/test'] = {code: 'context.set("BlockHeight", block.header().height);'};
         const block_height = 85376546;
         const mutations = await indexer.runFunctions(block_height, functions);
-        expect(mutations).toEqual({"keysValues": {"BlockHeight": 85376546}, "mutations": [], "variables": {}});
+        expect(mutations[0]).toEqual({"keysValues": {"BlockHeight": 85376546}, "mutations": [], "variables": {}});
     });
 
     test('Indexer.runFunctions() should execute a test function against a given block using a full mutation to write to key-value storage', async () => {
         const indexer = new Indexer('mainnet', 'us-west-2');
         const functions = {};
-        functions['buildnear.testnet/itest3'] = {code: 'context.graphql.mutation(`mutation { insert_indexer_storage_one(object: {function_name: "buildnear.testnet/itest3", key_name: "BlockHeight", value: "${block.header().height}"} on_conflict: {constraint: indexer_storage_pkey, update_columns: value}) {key_name}}`);'};
+        functions['buildnear.testnet/itest3'] = {code: 'context.graphql(`mutation { insert_indexer_storage_one(object: {function_name: "buildnear.testnet/itest3", key_name: "BlockHeight", value: "${block.header().height}"} on_conflict: {constraint: indexer_storage_pkey, update_columns: value}) {key_name}}`);'};
         const block_height = 85376546;
         const mutations = await indexer.runFunctions(block_height, functions);
-        expect(mutations).toBeDefined()
-        expect(JSON.stringify(mutations)).toContain("insert_indexer_storage_one");
+        expect(mutations[0]).toBeDefined()
+        expect(JSON.stringify(mutations[0])).toContain("insert_indexer_storage_one");
     });
 
     /** Note that the on_conflict block in the mutation is for test repeatability.
@@ -84,8 +84,8 @@ describe('Indexer integration tests', () => {
                     const mutationData = { post: { account_id: accountId, block_height: postData.block_height.toString(),
                       block_timestamp: postData.block_timestamp, receipt_id: postData.receipt_id, 
                       content: postData.post}};
-                    context.graphql.mutation('mutation createPost($post:posts_insert_input!) { insert_posts_one(object: $post on_conflict: {constraint: posts_account_id_block_height_key, update_columns: content}) { id } }');
-                    context.graphql.allVariables(mutationData);
+                    context.graphql('mutation createPost($post:posts_insert_input!) { insert_posts_one(object: $post on_conflict: {constraint: posts_account_id_block_height_key, update_columns: content}) { id } }',
+                        mutationData);
                 }
             });
         }
@@ -94,8 +94,8 @@ describe('Indexer integration tests', () => {
         const block_height = 85242526; // post,  // 84940247; // comment
         const returnValue = await indexer.runFunctions(block_height, functions);
 
-        expect(returnValue.mutations.length).toEqual(1);
-        expect(returnValue.mutations[0]).toContain("mutation createPost($post:posts_insert_input!) { insert_posts_one(object: $post on_conflict: {constraint: posts_account_id_block_height_key, update_columns: content}) { id } }");
+        expect(returnValue[0].mutations.length).toEqual(1);
+        expect(returnValue[0].mutations[0]).toContain("mutation createPost($post:posts_insert_input!) { insert_posts_one(object: $post on_conflict: {constraint: posts_account_id_block_height_key, update_columns: content}) { id } }");
     });
 
     /** Note that the on_conflict block in the mutation is for test repeatability.
@@ -147,6 +147,29 @@ describe('Indexer integration tests', () => {
             '  }\n' +
             '}')
         expect(valueSet.comments[0].post_id).toEqual(2);
+    });
+
+    test("writeLog() should write a log to the database", async () => {
+        const indexer = new Indexer('mainnet', 'us-west-2');
+        const id = await indexer.writeLog("buildnear.testnet/itest", 85376002, "test message");
+        expect(id).toBeDefined();
+        expect(id.length).toBe(36);
+    });
+
+    test("fetchIndexerFunctions() should fetch the indexer functions from the database", async () => {
+        const indexer = new Indexer('mainnet', 'us-west-2');
+        const functions = await indexer.fetchIndexerFunctions();
+        console.log(functions);
+        expect(functions).toBeDefined();
+        expect(Object.keys(functions).length).toBeGreaterThan(0);
+    });
+    // todo test indexer.runFunctions() with a function that has a bad graphql mutation
+
+    test("writeFunctionState should write a function state to the database", async () => {
+        const indexer = new Indexer('mainnet', 'us-west-2');
+        const result = await indexer.writeFunctionState("buildnear.testnet/itest8", 85376002);
+        expect(result).toBeDefined();
+        expect(result.insert_indexer_state.returning[0].current_block_height).toBe(85376002);
     });
 });
 
