d6369ef70890256e9133239a49c43281737c7850
Roshaan Siddiqui <siddiqui.roshaan@gmail.com>
2023-04-10 15:50:26 -0500

    cleanup

--- query-api-react/src/components/CreateNewIndexer/CreateNewIndexer.js
+++ query-api-react/src/components/CreateNewIndexer/CreateNewIndexer.js
@@ -1,29 +1,13 @@
-import InputGroup from 'react-bootstrap/InputGroup';
-
 import Editor from '../../components/Editor';
 
-
-
-
 const CreateNewIndexer = (props) => {
-
-    // Info about how to write inside SQL editor
-    const info = "Use the following editor to specify tables and their columns you would like to create. Once you register your function, you wil NOT be able to change this. "
-
-    return (
-        <>
-            {/* <div className="container"> */}
-            <Editor
-                accountId={props.accountId}
-                options={{ create_new_indexer: true }}
-                actionButtonText="Create New Indexer"
-                theme="vs-dark"
-            />
-            {/* </div> */}
-
-        </>
-
-    )
+  return (
+    <Editor
+      accountId={props.accountId}
+      options={{ create_new_indexer: true }}
+      actionButtonText="Create New Indexer"
+      theme="vs-dark"
+    />);
 };
 
 export default CreateNewIndexer;
