95121d2cb73af60c72c4e2a2f378a8101420a525
Morgan McCauley <morgan@mccauley.co.nz>
2023-05-04 11:09:04 +1200

    fix: Use `opts` reference to construct lake config

--- indexer/shared/src/lib.rs
+++ indexer/shared/src/lib.rs
@@ -148,7 +148,7 @@ impl Opts {
 }
 
 impl Opts {
-    pub async fn to_lake_config(self) -> near_lake_framework::LakeConfig {
+    pub async fn to_lake_config(&self) -> near_lake_framework::LakeConfig {
         let s3_config = aws_sdk_s3::config::Builder::from(&self.lake_aws_sdk_config()).build();
 
         let config_builder = near_lake_framework::LakeConfigBuilder::default().s3_config(s3_config);
@@ -156,10 +156,10 @@ impl Opts {
         match &self.chain_id {
             ChainId::Mainnet(_) => config_builder
                 .mainnet()
-                .start_block_height(get_start_block_height(&self).await),
+                .start_block_height(get_start_block_height(self).await),
             ChainId::Testnet(_) => config_builder
                 .testnet()
-                .start_block_height(get_start_block_height(&self).await),
+                .start_block_height(get_start_block_height(self).await),
         }
         .build()
         .expect("Failed to build LakeConfig")
