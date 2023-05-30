9b37888523c0f71b6ecc9dc72f78fae36c54a738
Roshaan Siddiqui <siddiqui.roshaan@gmail.com>
2023-05-30 11:26:36 -0400

    test: upload widgets to dev

--- frontend/widgets/examples/feed/src/QueryApi.dev.Feed.jsx
+++ frontend/widgets/examples/feed/src/QueryApi.dev.Feed.jsx
@@ -2,6 +2,7 @@ const GRAPHQL_ENDPOINT =
   "https://queryapi-hasura-graphql-24ktefolwq-ew.a.run.app";
 const APP_OWNER = "dev-queryapi.dataplatform.near";
 
+
 return (
   <Widget
     src={`${APP_OWNER}/widget/QueryApi.Examples.Feed`}
