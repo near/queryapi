5e4f24c5668e1e3c4db9e6d026e593447cd4da72
Roshaan Siddiqui <siddiqui.roshaan@gmail.com>
2023-05-31 09:59:09 -0400

    Feat: Prod Social Feed Widget (#80)

---
+++ frontend/widgets/examples/feed/src/QueryApi.Feed.jsx
@@ -0,0 +1,13 @@
+const GRAPHQL_ENDPOINT =
+  "https://queryapi-hasura-graphql-24ktefolwq-ew.a.run.app";
+const APP_OWNER = "dataplatform.near";
+
+return (
+  <Widget
+    src={`${APP_OWNER}/widget/QueryApi.Examples.Feed`}
+    props={{
+      GRAPHQL_ENDPOINT,
+      APP_OWNER,
+    }}
+  />
+);
