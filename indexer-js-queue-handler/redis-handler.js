db042ef0841b6698fa18e349354052f76c5bb9ee
Morgan McCauley <morgan@mccauley.co.nz>
2023-06-16 11:29:02 +1200

    refactor: Split logic in to separate functions

--- indexer-js-queue-handler/redis-handler.js
+++ indexer-js-queue-handler/redis-handler.js
@@ -6,55 +6,62 @@ const client = createClient({ url: process.env.REDIS_CONNECTION_STRING });
 const indexer = new Indexer("mainnet");
 
 const BATCH_SIZE = 1;
+const DEFAULT_ID = "0";
 
 client.on("error", (err) => console.log("Redis Client Error", err));
 
 await client.connect();
 
+const runFunction = async (message) => {
+    const functions = {};
+    const functionName = message.account_id + "/" + message.function_name;
+    functions[functionName] = {
+        account_id: message.account_id,
+        function_name: message.function_name,
+        code: message.code,
+        schema: message.schema,
+        provisioned: false,
+    };
+
+    try {
+        await indexer.runFunctions(Number(message.block_height), functions, false, {
+            imperative: true,
+            provision: true,
+        });
+        console.log(`Success: ${functionName}`);
+    } catch (err) {
+        console.log(`Failed: ${functionName}`, err);
+    }
+};
+
 // should probably store this in redis
-const lastIdByAccount = {};
+const lastIdByIndexer = {};
+
+const getLatestMessageFromStream = async (indexerName) => {
+    const id = lastIdByIndexer[indexerName] ?? DEFAULT_ID;
+
+    const results = await client.xRead({ key: indexerName, id }, { COUNT: 1 });
+
+    if (!results) {
+        return null;
+    }
+
+    const lastId = results[0].messages[0].id;
+    lastIdByIndexer[indexerName] = lastId;
+
+    const { message } = results[0].messages[0];
+
+    return message
+};
 
 while (true) {
-    const indexer_names = await client.sMembers("indexers");
+    const indexers = await client.sMembers("indexers");
 
     await Promise.all(
-        indexer_names.map(async (indexer_name) => {
-            const id = lastIdByAccount[indexer_name] ?? "0";
-
-            const results = await client.xRead(
-                { key: indexer_name, id },
-                { COUNT: 1 }
-            );
-
-            if (!results) {
-                return;
-            }
-
-            const lastId = results[0].messages[0].id;
-            const { message } = results[0].messages[0];
-
-            lastIdByAccount[indexer_name] = lastId;
-
-            const functions = {};
-            const function_name = message.account_id + "/" + message.function_name;
-            functions[function_name] = {
-                account_id: message.account_id,
-                function_name: message.function_name,
-                code: message.code,
-                schema: message.schema,
-                provisioned: false,
-            };
-
-            try {
-                await indexer.runFunctions(
-                    Number(message.block_height),
-                    functions,
-                    false,
-                    { imperative: true, provision: true }
-                );
-                console.log(`Success: ${indexer_name}`);
-            } catch (err) {
-                console.log(`Failed: ${indexer_name}`, err);
+        indexers.map(async (indexer_name) => {
+            const message = await getLatestMessageFromStream(indexer_name);
+            if (message) {
+                await runFunction(message);
             }
         })
     );
