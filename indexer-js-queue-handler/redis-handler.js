4865e2ad3192c5e904770f366e43c4c29e6e0339
Morgan McCauley <morgan@mccauley.co.nz>
2023-06-16 09:56:03 +1200

    feat: Batch redis stream processing

--- indexer-js-queue-handler/redis-handler.js
+++ indexer-js-queue-handler/redis-handler.js
@@ -5,39 +5,47 @@ import Indexer from "./indexer.js";
 const client = createClient({ url: process.env.REDIS_CONNECTION_STRING });
 const indexer = new Indexer('mainnet')
 
+const BATCH_SIZE = 10
+
 client.on('error', err => console.log('Redis Client Error', err));
 
 await client.connect();
 
-let lastId = '$';
+let lastId = '0';
 
 while (true) {
     const results = await client.xRead(
         { key: 'functions', id: lastId },
-        { BLOCK: 0, COUNT: 1 }
+        { /* BLOCK: 0, */ COUNT: BATCH_SIZE }
     );
 
-    lastId = results[0].messages[0].id;
-
-    const message = results[0].messages[0].message;
-
-    const functions = {};
-
-    const function_name = message.account_id + '/' + message.function_name;
-    functions[function_name] = {
-        account_id: message.account_id,
-        function_name: message.function_name,
-        code: message.code,
-        schema: message.schema,
-        provisioned: false,
-    };
-
-    try {
-        await indexer.runFunctions(Number(message.block_height), functions, false, {imperative: true, provision: true});
-        console.log(`Success: ${message.account_id}/${message.function_name}`)
-    } catch {
-        console.log(`Failed: ${message.account_id}/${message.function_name}`)
+    if (!results) {
+        break;
     }
+
+    const { messages } = results[0];
+
+    lastId = messages[messages.length - 1].id;
+
+    await Promise.all(messages.map(async ({ message }) => {
+        const functions = {};
+
+        const function_name = message.account_id + '/' + message.function_name;
+        functions[function_name] = {
+            account_id: message.account_id,
+            function_name: message.function_name,
+            code: message.code,
+            schema: message.schema,
+            provisioned: false,
+        };
+
+        try {
+            await indexer.runFunctions(Number(message.block_height), functions, false, {imperative: true, provision: true});
+            console.log(`Success: ${message.account_id}/${message.function_name}`)
+        } catch(err) {
+            console.log(`Failed: ${message.account_id}/${message.function_name}`, err)
+        }
+    }));
 }
 
 await client.disconnect();
