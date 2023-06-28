621c082d0dbd0f8e7efd381cc41be32e6abd1708
Morgan McCauley <morgan@mccauley.co.nz>
2023-06-29 09:03:59 +1200

    [DPLT-1044] Write latest near.org post block height to Grafana (#112)

---
+++ indexer-js-queue-handler/latest-post-metrics-writer.js
@@ -0,0 +1,24 @@
+import fetch from "node-fetch";
+import AWS from "aws-sdk";
+
+import Metrics from "./metrics";
+
+export const handler = async () => {
+    const metrics = new Metrics("QueryAPI");
+
+    const response = await fetch("https://api.near.social/index", {
+        method: "POST",
+        body: JSON.stringify({
+            action: "post",
+            key: "main",
+            options: {
+                limit: 1,
+                order: "desc",
+            },
+        }),
+    });
+
+    const [{ blockHeight }] = await response.json();
+
+    await metrics.putBlockHeight("social.near", "posts", blockHeight);
+};
