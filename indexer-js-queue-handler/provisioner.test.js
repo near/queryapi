0c2a6e4f612bea98755dbf649888e2ce15cce02b
Morgan Mccauley <morgan@mccauley.co.nz>
2023-07-17 16:06:17 +1200

    test: Test separate db per user provisioning

--- indexer-js-queue-handler/provisioner.test.js
+++ indexer-js-queue-handler/provisioner.test.js
@@ -4,7 +4,39 @@ import VError from 'verror'
 import HasuraClient from './hasura-client';
 import Provisioner from './provisioner';
 
-describe('Provision', () => {
+describe('Provisioner', () => {
+    let pgPool;
+    let pgClient;
+    let hasuraClient;
+
+    const tableNames = ['blocks'];
+    const schemaName = 'public';
+    const userName = 'morgs_near';
+    const roleName = 'morgs_near';
+    const databaseName = 'morgs_near';
+    const databaseSchema = 'CREATE TABLE blocks (height numeric)';
+    const error = new Error('some error');
+
+
+    beforeEach(() => {
+        hasuraClient = {
+            getTableNames: jest.fn().mockReturnValueOnce(tableNames),
+            trackTables: jest.fn().mockReturnValueOnce(),
+            trackForeignKeyRelationships: jest.fn().mockReturnValueOnce(),
+            addPermissionsToTables: jest.fn().mockReturnValueOnce(),
+            addDatasource: jest.fn().mockReturnValueOnce(),
+            runSql: jest.fn().mockReturnValueOnce(),
+        };
+
+        pgClient = {
+            query: jest.fn().mockResolvedValue(),
+            release: jest.fn().mockResolvedValue(),
+        };
+        pgPool = {
+            connect: jest.fn().mockResolvedValue(pgClient)
+        };
+    });
+
     it('checks if the endpoint already exists', async () => {
         const provisioner = new Provisioner({
             isSchemaCreated: jest.fn().mockResolvedValueOnce(true)
@@ -13,29 +45,24 @@ describe('Provision', () => {
         expect(await provisioner.doesEndpointExist('schema')).toBe(true);
     });
 
-    it('creates an authenticated endpoint', async () => {
-        const tableNames = ['blocks'];
-        const hasuraClient = {
-            createSchema: jest.fn().mockReturnValueOnce(),
-            runMigrations: jest.fn().mockReturnValueOnce(),
-            getTableNames: jest.fn().mockReturnValueOnce(tableNames),
-            trackTables: jest.fn().mockReturnValueOnce(),
-            trackForeignKeyRelationships: jest.fn().mockReturnValueOnce(),
-            addPermissionsToTables: jest.fn().mockReturnValueOnce(),
-        };
-        const provisioner = new Provisioner(hasuraClient);
+    it('provisions an API for the user', async () => {
+        const provisioner = new Provisioner(hasuraClient, pgPool);
 
-        const schemaName = 'schema';
-        const roleName = 'role';
-        const migration = 'CREATE TABLE blocks (height numeric)';
-        await provisioner.createAuthenticatedEndpoint(schemaName, roleName, migration);
+        await provisioner.provisionUserApi(userName, databaseSchema);
 
-        expect(hasuraClient.createSchema).toBeCalledWith(schemaName);
-        expect(hasuraClient.runMigrations).toBeCalledWith(schemaName, migration);
-        expect(hasuraClient.getTableNames).toBeCalledWith(schemaName);
-        expect(hasuraClient.trackTables).toBeCalledWith(schemaName, tableNames);
+        expect(pgClient.query.mock.calls).toEqual([
+            ['CREATE DATABASE morgs_near', []],
+            ['CREATE USER morgs_near WITH PASSWORD \'password\';', []],
+            ['GRANT ALL PRIVILEGES ON DATABASE morgs_near TO morgs_near;', []],
+            ['REVOKE CONNECT ON DATABASE morgs_near FROM PUBLIC;', []],
+        ]);
+        expect(hasuraClient.addDatasource).toBeCalledWith(userName, 'password', databaseName);
+        expect(hasuraClient.runSql).toBeCalledWith(databaseName, databaseSchema);
+        expect(hasuraClient.getTableNames).toBeCalledWith(schemaName, databaseName);
+        expect(hasuraClient.trackTables).toBeCalledWith(schemaName, tableNames, databaseName);
         expect(hasuraClient.addPermissionsToTables).toBeCalledWith(
             schemaName,
+            databaseName,
             tableNames,
             roleName,
             [
@@ -47,135 +74,59 @@ describe('Provision', () => {
         );
     });
 
-    it('throws an error when it fails to create the schema', async () => {
-        const error = new Error('some http error');
-        const hasuraClient = {
-            createSchema: jest.fn().mockRejectedValue(error),
-        };
-        const provisioner = new Provisioner(hasuraClient);
+    it('throws an error when it fails to create a postgres db', async () => {
+        pgClient.query = jest.fn().mockRejectedValue(error);
 
-        try {
-            await provisioner.createAuthenticatedEndpoint('name', 'role', 'CREATE TABLE blocks (height numeric)')
-        } catch (error) {
-            expect(error.message).toBe('Failed to provision endpoint: Failed to create schema: some http error');
-            expect(VError.info(error)).toEqual({
-                schemaName: 'name',
-                roleName: 'role',
-                migration: 'CREATE TABLE blocks (height numeric)',
-            });
-        }
+        const provisioner = new Provisioner(hasuraClient, pgPool);
+
+        await expect(provisioner.provisionUserApi(userName, databaseSchema)).rejects.toThrow('Failed to provision endpoint: Failed to create user db: some error');
+    });
+
+    it('throws an error when it fails to add the db to hasura', async () => {
+        hasuraClient.addDatasource = jest.fn().mockRejectedValue(error);
+
+        const provisioner = new Provisioner(hasuraClient, pgPool);
+
+        await expect(provisioner.provisionUserApi(userName, databaseSchema)).rejects.toThrow('Failed to provision endpoint: Failed to add datasource: some error');
     });
 
     it('throws an error when it fails to run migrations', async () => {
-        const error = new Error('some http error');
-        const hasuraClient = {
-            createSchema: jest.fn().mockReturnValueOnce(),
-            runMigrations: jest.fn().mockRejectedValue(error),
-        };
-        const provisioner = new Provisioner(hasuraClient);
+        hasuraClient.runSql = jest.fn().mockRejectedValue(error);
 
-        try {
-            await provisioner.createAuthenticatedEndpoint('name', 'role', 'CREATE TABLE blocks (height numeric)')
-        } catch (error) {
-            expect(error.message).toBe('Failed to provision endpoint: Failed to run migrations: some http error');
-            expect(VError.info(error)).toEqual({
-                schemaName: 'name',
-                roleName: 'role',
-                migration: 'CREATE TABLE blocks (height numeric)',
-            });
-        }
+        const provisioner = new Provisioner(hasuraClient, pgPool);
+
+        await expect(provisioner.provisionUserApi(userName, databaseSchema)).rejects.toThrow('Failed to provision endpoint: Failed to run migrations: some error');
     });
 
     it('throws an error when it fails to fetch table names', async () => {
-        const error = new Error('some http error');
-        const hasuraClient = {
-            createSchema: jest.fn().mockReturnValueOnce(),
-            runMigrations: jest.fn().mockReturnValueOnce(),
-            getTableNames: jest.fn().mockRejectedValue(error),
-        };
-        const provisioner = new Provisioner(hasuraClient);
+        hasuraClient.getTableNames = jest.fn().mockRejectedValue(error);
 
-        try {
-            await provisioner.createAuthenticatedEndpoint('name', 'role', 'CREATE TABLE blocks (height numeric)')
-        } catch (error) {
-            expect(error.message).toBe('Failed to provision endpoint: Failed to fetch table names: some http error');
-            expect(VError.info(error)).toEqual({
-                schemaName: 'name',
-                roleName: 'role',
-                migration: 'CREATE TABLE blocks (height numeric)',
-            });
-        }
+        const provisioner = new Provisioner(hasuraClient, pgPool);
+
+        await expect(provisioner.provisionUserApi(userName, databaseSchema)).rejects.toThrow('Failed to provision endpoint: Failed to fetch table names: some error');
     });
 
     it('throws an error when it fails to track tables', async () => {
-        const error = new Error('some http error');
-        const tableNames = ['blocks'];
-        const hasuraClient = {
-            createSchema: jest.fn().mockReturnValueOnce(),
-            runMigrations: jest.fn().mockReturnValueOnce(),
-            getTableNames: jest.fn().mockReturnValueOnce(tableNames),
-            trackTables: jest.fn().mockRejectedValueOnce(error),
-        };
-        const provisioner = new Provisioner(hasuraClient);
+        hasuraClient.trackTables = jest.fn().mockRejectedValue(error);
 
-        try {
-            await provisioner.createAuthenticatedEndpoint('name', 'role', 'CREATE TABLE blocks (height numeric)')
-        } catch (error) {
-            expect(error.message).toBe('Failed to provision endpoint: Failed to track tables: some http error');
-            expect(VError.info(error)).toEqual({
-                schemaName: 'name',
-                roleName: 'role',
-                migration: 'CREATE TABLE blocks (height numeric)',
-            });
-        }
+        const provisioner = new Provisioner(hasuraClient, pgPool);
+
+        await expect(provisioner.provisionUserApi(userName, databaseSchema)).rejects.toThrow('Failed to provision endpoint: Failed to track tables: some error');
     });
 
     it('throws an error when it fails to track foreign key relationships', async () => {
-        const error = new Error('some http error');
-        const tableNames = ['blocks'];
-        const hasuraClient = {
-            createSchema: jest.fn().mockReturnValueOnce(),
-            runMigrations: jest.fn().mockReturnValueOnce(),
-            getTableNames: jest.fn().mockReturnValueOnce(tableNames),
-            trackTables: jest.fn().mockReturnValueOnce(),
-            trackForeignKeyRelationships: jest.fn().mockRejectedValueOnce(error),
-        };
-        const provisioner = new Provisioner(hasuraClient);
+        hasuraClient.trackForeignKeyRelationships = jest.fn().mockRejectedValue(error);
 
-        try {
-            await provisioner.createAuthenticatedEndpoint('name', 'role', 'CREATE TABLE blocks (height numeric)')
-        } catch (error) {
-            expect(error.message).toBe('Failed to provision endpoint: Failed to track foreign key relationships: some http error');
-            expect(VError.info(error)).toEqual({
-                schemaName: 'name',
-                roleName: 'role',
-                migration: 'CREATE TABLE blocks (height numeric)',
-            });
-        }
+        const provisioner = new Provisioner(hasuraClient, pgPool);
+
+        await expect(provisioner.provisionUserApi(userName, databaseSchema)).rejects.toThrow('Failed to provision endpoint: Failed to track foreign key relationships: some error');
     })
 
     it('throws an error when it fails to add permissions to tables', async () => {
-        const error = new Error('some http error');
-        const tableNames = ['blocks'];
-        const hasuraClient = {
-            createSchema: jest.fn().mockReturnValueOnce(),
-            runMigrations: jest.fn().mockReturnValueOnce(),
-            getTableNames: jest.fn().mockReturnValueOnce(tableNames),
-            trackTables: jest.fn().mockReturnValueOnce(),
-            trackForeignKeyRelationships: jest.fn().mockReturnValueOnce(),
-            addPermissionsToTables: jest.fn().mockRejectedValue(error),
-        };
-        const provisioner = new Provisioner(hasuraClient);
+        hasuraClient.addPermissionsToTables = jest.fn().mockRejectedValue(error);
 
-        try {
-            await provisioner.createAuthenticatedEndpoint('name', 'role', 'CREATE TABLE blocks (height numeric)')
-        } catch (error) {
-            expect(error.message).toBe('Failed to provision endpoint: Failed to add permissions to tables: some http error');
-            expect(VError.info(error)).toEqual({
-                migration: 'CREATE TABLE blocks (height numeric)',
-                schemaName: 'name',
-                roleName: 'role',
-            });
-        }
+        const provisioner = new Provisioner(hasuraClient, pgPool);
+
+        await expect(provisioner.provisionUserApi(userName, databaseSchema)).rejects.toThrow('Failed to provision endpoint: Failed to add permissions to tables: some error');
     });
 })
