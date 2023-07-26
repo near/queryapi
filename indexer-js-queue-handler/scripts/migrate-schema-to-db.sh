2211e193cb67ca3f7e7ad6fbb2d5ddd95a326e41
Morgan Mccauley <morgan@mccauley.co.nz>
2023-07-26 14:16:12 +1200

    feat: Create script to migrate indexer data

---
+++ indexer-js-queue-handler/scripts/migrate-schema-to-db.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+set -e
+
+export PGUSER=postgres
+export PGPASSWORD=postgrespassword
+export PGHOST=localhost
+export PGPORT=5432
+export PGDATABASE=postgres
+
+PASSWORD_LENGTH=12
+
+if [ $# -eq 0 ]
+then
+  echo "Please pass schema name as first argument"
+  exit 1
+fi
+
+SCHEMA=$1 
+echo "Processing schema $SCHEMA..."
+
+ACCOUNT=${SCHEMA%_near_*}
+INDEXER=${SCHEMA#*_near_}
+
+DATABASE="${ACCOUNT}_near"
+USER="$DATABASE"
+PASSWORD=$(openssl rand -base64 $PASSWORD_LENGTH | tr -d '/+' | cut -c 1-$PASSWORD_LENGTH)
+
+echo "Creating user $USER with password $PASSWORD"
+psql --command="CREATE USER \"$USER\" WITH PASSWORD '$PASSWORD';"
+
+echo "Creating database $DATABASE..."
+createdb --owner="$USER" --template=template0 "$DATABASE"
+
+echo "Restricting connection to database $DATABASE to $USER..."
+psql --command="REVOKE CONNECT ON DATABASE \"$DATABASE\" FROM PUBLIC;"
+psql --command="GRANT ALL PRIVILEGES ON DATABASE \"$DATABASE\" TO \"$USER\";"
+
+echo "Dumping schema $SCHEMA..."
+pg_dump --schema=$SCHEMA --file="$SCHEMA.sql"
+
+echo "Restoring schema $SCHEMA in $DATABASE..."
+psql --dbname=$DATABASE < "$SCHEMA.sql"
+
+echo "Renaming schema to $INDEXER..."
+psql --dbname=$DATABASE --command="ALTER SCHEMA \"$SCHEMA\" RENAME TO \"$INDEXER\";"
+
+echo "Changing schema ownership to $USER..."
+psql --dbname=$DATABASE --command "
+  ALTER SCHEMA \"$INDEXER\" OWNER TO \"$USER\";
+  ALTER DEFAULT PRIVILEGES IN SCHEMA \"$INDEXER\" GRANT ALL ON TABLES TO \"$USER\";
+"
+
+echo "Changing ownership of all tables in $INDEXER to $USER"
+psql --dbname=$DATABASE <<EOF
+DO
+\$\$
+DECLARE 
+    table_name text;
+    l_schema text := '$INDEXER';
+    l_user text := '$USER';
+BEGIN 
+    FOR table_name IN (SELECT tablename FROM pg_tables WHERE schemaname = l_schema)
+    LOOP 
+        RAISE NOTICE 'Altering table: %', table_name;
+        EXECUTE format('ALTER TABLE %I.%I OWNER TO %I', l_schema, table_name, l_user); 
+    END LOOP;
+END 
+\$\$
+EOF
