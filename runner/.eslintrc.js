75d9e6f2abc0df92a6fcb1201e7b61be4004135c
Morgan Mccauley <morgan@mccauley.co.nz>
2023-08-01 14:58:12 +1200

    feat: Bootstrap `runner` repo

---
+++ runner/.eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  env: {
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'standard-with-typescript'
+  ],
+  overrides: [
+    {
+      files: ['*.js', '*.jsx', '*.ts', '*.tsx'],
+      parserOptions: {
+        project: './tsconfig.json'
+      }
+    }
+  ],
+  rules: {
+    '@typescript-eslint/semi': ['error', 'always'],
+    '@typescript-eslint/comma-dangle': ['error', 'only-multiline']
+  }
+};
