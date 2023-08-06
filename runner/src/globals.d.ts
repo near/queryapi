7f85607dc05e7970a8102825bdf1a87597952ab9
Morgan Mccauley <morgan@mccauley.co.nz>
2023-08-07 09:51:02 +1200

    refactor: Use default `node-pg` env vars

--- runner/src/globals.d.ts
+++ runner/src/globals.d.ts
@@ -2,11 +2,11 @@ declare namespace NodeJS {
   export interface ProcessEnv {
     HASURA_ENDPOINT: string
     HASURA_ADMIN_SECRET: string
-    PG_HOST: string
-    PG_PORT: string
-    PG_ADMIN_USER: string
-    PG_ADMIN_PASSWORD: string
-    PG_ADMIN_DATABASE: string
+    PGHOST: string
+    PGPORT: string
+    PGUSER: string
+    PGPASSWORD: string
+    PGDATABASE: string
     REGION: string
   }
 }
