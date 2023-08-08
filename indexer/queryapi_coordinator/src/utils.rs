98bc8bbceb77748d2dbcf741f7e665560addae5f
Morgan McCauley <morgan@mccauley.co.nz>
2023-08-08 20:57:04 +1200

    Revert "DPLT-1129 Publish unprocessed stream messages count to Grafana" (#171)

--- indexer/queryapi_coordinator/src/utils.rs
+++ indexer/queryapi_coordinator/src/utils.rs
@@ -1,7 +1,5 @@
-use crate::metrics;
-
 pub(crate) async fn stats(redis_connection_manager: storage::ConnectionManager) {
-    let interval_secs = 5;
+    let interval_secs = 10;
     let mut previous_processed_blocks: u64 =
         storage::get::<u64>(&redis_connection_manager, "blocks_processed")
             .await
@@ -48,34 +46,6 @@ pub(crate) async fn stats(redis_connection_manager: storage::ConnectionManager) 
             alert_rules_count,
         );
         previous_processed_blocks = processed_blocks;
-
-        let streams = storage::smembers(&redis_connection_manager, storage::INDEXER_SET_KEY)
-            .await
-            .unwrap_or(Vec::new());
-
-        for stream in streams {
-            let latest_id = storage::get::<String>(
-                &redis_connection_manager,
-                storage::generate_stream_last_id_key(&stream),
-            )
-            .await
-            .unwrap_or(storage::STREAM_SMALLEST_ID.to_string());
-
-            let unprocessed_message_count = storage::xrange(
-                &redis_connection_manager,
-                storage::generate_stream_key(&stream),
-                &latest_id,
-                storage::STREAM_LARGEST_ID,
-            )
-            .await
-            .unwrap_or(Vec::new())
-            .len() as i64;
-
-            metrics::UNPROCESSED_STREAM_MESSAGES
-                .with_label_values(&[&stream])
-                .set(unprocessed_message_count);
-        }
-
         tokio::time::sleep(std::time::Duration::from_secs(interval_secs)).await;
     }
 }
