4c69e13e49351d73d92a26debf5360e4bdf64afb
Roshaan Siddiqui <siddiqui.roshaan@gmail.com>
2023-08-16 07:57:16 -0500

    test: test ci

--- frontend/widgets/src/QueryApi.App.jsx
+++ frontend/widgets/src/QueryApi.App.jsx
@@ -2,6 +2,7 @@ const view = props.view;
 const path = props.path;
 const tab = props.tab;
 const selectedIndexerPath = props.selectedIndexerPath;
+console.log(`${REPL_ACCOUNT_ID}`, "loaded the account")
 
 return (
   <Widget
