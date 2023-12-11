d91d7afa12e3028ee48df56901b080620b885aff
Morgan Mccauley <morgan@mccauley.co.nz>
2023-12-11 20:40:39 +1300

    test: Fix outcome reducer tests

--- block-streamer/src/test_utils.rs
+++ block-streamer/src/test_utils.rs
@@ -1,7 +1,8 @@
 use aws_smithy_runtime::client::http::test_util::{ReplayEvent, StaticReplayClient};
 use aws_smithy_types::body::SdkBody;
+use near_lake_framework::near_indexer_primitives;
 
-fn generate_replay_events_for_block(block_height: u32) -> Vec<ReplayEvent> {
+fn generate_replay_events_for_block(block_height: u64) -> Vec<ReplayEvent> {
     vec![
         ReplayEvent::new(
             http::Request::builder()
@@ -73,7 +74,7 @@ fn generate_replay_events_for_block(block_height: u32) -> Vec<ReplayEvent> {
 }
 
 /// Responds with an invalid block - forcing `near_lake_framework` to exit
-fn generate_stop_replay_event_for_block(block_height: u32) -> Vec<ReplayEvent> {
+fn generate_stop_replay_event_for_block(block_height: u64) -> Vec<ReplayEvent> {
     vec![
         ReplayEvent::new(
             http::Request::builder()
@@ -100,7 +101,7 @@ fn generate_stop_replay_event_for_block(block_height: u32) -> Vec<ReplayEvent> {
     ]
 }
 
-fn generate_replay_events_for_blocks(block_heights: &[u32]) -> Vec<ReplayEvent> {
+fn generate_replay_events_for_blocks(block_heights: &[u64]) -> Vec<ReplayEvent> {
     let mut events = Vec::new();
     for block_height in block_heights {
         events.extend(generate_replay_events_for_block(*block_height));
@@ -114,7 +115,7 @@ fn generate_replay_events_for_blocks(block_heights: &[u32]) -> Vec<ReplayEvent> 
 /// Creates `S3Config` with a mock HTTP client that simulates responses from S3. `block_heights`
 /// will be read from the top-level `data/` directory. `near_lake_framework` verifies the order of
 /// blocks, therefore passed `block_heights` _must_ be in the order which they were finalized.
-pub fn create_mock_lake_s3_config(block_heights: &[u32]) -> aws_sdk_s3::Config {
+pub fn create_mock_lake_s3_config(block_heights: &[u64]) -> aws_sdk_s3::Config {
     let replay_events = generate_replay_events_for_blocks(block_heights);
 
     let replay_client = StaticReplayClient::new(replay_events);
@@ -124,4 +125,51 @@ pub fn create_mock_lake_s3_config(block_heights: &[u32]) -> aws_sdk_s3::Config {
         .region(aws_sdk_s3::config::Region::new("eu-central-1"))
         .http_client(replay_client)
         .build()
+}
+
+pub fn get_streamer_message(block_height: u64) -> near_indexer_primitives::StreamerMessage {
+    let block: near_indexer_primitives::views::BlockView = serde_json::from_slice(
+        &std::fs::read(format!(
+            "{}/data/{block_height:0>12}/block.json",
+            env!("CARGO_MANIFEST_DIR")
+        ))
+        .unwrap(),
+    )
+    .unwrap();
+    let shards: Vec<near_indexer_primitives::IndexerShard> = vec![
+        serde_json::from_slice(
+            &std::fs::read(format!(
+                "{}/data/{block_height:0>12}/shard_0.json",
+                env!("CARGO_MANIFEST_DIR")
+            ))
+            .unwrap(),
+        )
+        .unwrap(),
+        serde_json::from_slice(
+            &std::fs::read(format!(
+                "{}/data/{block_height:0>12}/shard_1.json",
+                env!("CARGO_MANIFEST_DIR")
+            ))
+            .unwrap(),
+        )
+        .unwrap(),
+        serde_json::from_slice(
+            &std::fs::read(format!(
+                "{}/data/{block_height:0>12}/shard_2.json",
+                env!("CARGO_MANIFEST_DIR")
+            ))
+            .unwrap(),
+        )
+        .unwrap(),
+        serde_json::from_slice(
+            &std::fs::read(format!(
+                "{}/data/{block_height:0>12}/shard_3.json",
+                env!("CARGO_MANIFEST_DIR")
+            ))
+            .unwrap(),
+        )
+        .unwrap(),
+    ];
+
+    near_indexer_primitives::StreamerMessage { block, shards }
 }
