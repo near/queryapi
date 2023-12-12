c2f857ba07cbf934080ab2c825f805a459eaeff4
Morgan Mccauley <morgan@mccauley.co.nz>
2023-12-12 14:54:56 +1300

    feat: Fetch and print registry contract

--- coordinator/src/main.rs
+++ coordinator/src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use near_jsonrpc_client::methods::query::RpcQueryRequest;
+use near_jsonrpc_client::JsonRpcClient;
+use near_jsonrpc_primitives::types::query::QueryResponseKind;
+use near_primitives::types::{BlockReference, Finality, FunctionArgs};
+use near_primitives::views::QueryRequest;
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let json_rpc_client = JsonRpcClient::connect("https://rpc.mainnet.near.org");
+
+    let response = json_rpc_client
+        .call(RpcQueryRequest {
+            block_reference: BlockReference::Finality(Finality::Final),
+            request: QueryRequest::CallFunction {
+                method_name: "list_indexer_functions".to_string(),
+                account_id: "queryapi.dataplatform.near".to_string().try_into().unwrap(),
+                args: FunctionArgs::from("{}".as_bytes().to_vec()),
+            },
+        })
+        .await?;
+
+    if let QueryResponseKind::CallResult(call_result) = response.kind {
+        let registry: serde_json::Value = serde_json::from_slice(&call_result.result)?;
+        eprintln!("registry = {:#?}", registry);
+    }
+
+    Ok(())
 }
