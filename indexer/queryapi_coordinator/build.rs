98be7cb12ed79e4814573554e5dcf722d2d50e77
Darun Seethammagari <darunrs@gmail.com>
2024-01-04 14:47:49 -0800

    Fix Docker issues

--- indexer/queryapi_coordinator/build.rs
+++ indexer/queryapi_coordinator/build.rs
@@ -1,5 +1,9 @@
 // TODO: Move to New Coordinator Folder
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::compile_protos("../../runner/protos/runner.proto")?;
+    // TODO: Update Runner Env deployment variables and docker compose file to contain RUNNER_PROTO_PATH
+    let proto_path = std::env::var("RUNNER_PROTO_PATH")
+        .unwrap_or_else(|_| "../../runner/protos/runner.proto".to_string());
+    // TODO: Remove .ok() when we expect tonic build to succeed in deployment
+    tonic_build::compile_protos(proto_path).ok();
     Ok(())
 }
