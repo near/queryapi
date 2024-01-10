dfcc15dbec4ebb75d8da1419fe0534df41714380
Juan Luis Santana <juanluis.personal@gmail.com>
2024-01-10 15:59:22 -0500

    Update frontend/src/constants/Strings.js
    Co-authored-by: Roshaan Siddiqui <siddiqui.roshaan@gmail.com>

--- frontend/src/constants/Strings.js
+++ frontend/src/constants/Strings.js
@@ -3,7 +3,7 @@ export const CODE_GENERAL_ERROR_MESSAGE = "There is an error in your code. Pleas
 export const CODE_FORMATTING_ERROR_MESSAGE = "There was an error while formatting your code. Please check the console for more details.";
 export const SCHEMA_TYPE_GENERATION_ERROR_MESSAGE = "There was an error while generating types for your SQL schema. Please ensure your schema is valid SQL DDL.";
 export const SCHEMA_FORMATTING_ERROR_MESSAGE = "There was an error while formatting your schema. Please, check the console for more details."
-export const INDEXER_REGISTER_TYPE_GENERATION_ERROR = "There was an error generating types from your schema, so the contextDB object cannot be created. Would you like to proceed with registering the Indexer despite this?";
+export const INDEXER_REGISTER_TYPE_GENERATION_ERROR = "There was an error generating types from your schema, so the context.db object cannot be created. Would you like to proceed with registering the Indexer despite this?";
 
 //error types
 export const FORMATTING_ERROR_TYPE = 'formatting_error';
