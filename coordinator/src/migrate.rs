0d65508d011c7d90986d79dd1edbcd742a7142cb
Morgan Mccauley <morgan@mccauley.co.nz>
2024-02-01 11:29:31 +1300

    refactor(migration): Extract migration steps to own functions

--- coordinator/src/migrate.rs
+++ coordinator/src/migrate.rs
@@ -1,6 +1,6 @@
 use crate::executors_handler::ExecutorsHandler;
 use crate::redis::RedisClient;
-use crate::registry::IndexerRegistry;
+use crate::registry::{IndexerConfig, IndexerRegistry};
 use crate::Allowlist;
 
 pub async fn migrate_pending_indexers(
@@ -24,67 +24,92 @@ pub async fn migrate_pending_indexers(
         let indexers = indexers.unwrap();
 
         for (_, indexer_config) in indexers.iter() {
-            // TODO should probably check if these exist?
+            remove_v1_control(redis_client, indexer_config).await?;
+            stop_v1_executors(executors_handler, indexer_config).await?;
+            merge_streams(redis_client, indexer_config).await?;
+        }
+    }
+
+    Ok(())
+}
+
+async fn remove_v1_control(
+    redis_client: &RedisClient,
+    indexer_config: &IndexerConfig,
+) -> anyhow::Result<()> {
+    // TODO should probably check if these exist?
+    redis_client
+        .srem(
+            RedisClient::STREAMS_SET,
+            indexer_config.get_real_time_redis_stream(),
+        )
+        .await?;
+    redis_client
+        .srem(
+            RedisClient::STREAMS_SET,
+            indexer_config.get_historical_redis_stream(),
+        )
+        .await?;
+
+    Ok(())
+}
+
+async fn stop_v1_executors(
+    executors_handler: &ExecutorsHandler,
+    indexer_config: &IndexerConfig,
+) -> anyhow::Result<()> {
+    executors_handler
+        .stop(indexer_config.get_real_time_redis_stream())
+        .await?;
+    executors_handler
+        .stop(indexer_config.get_historical_redis_stream())
+        .await?;
+
+    Ok(())
+}
+
+async fn merge_streams(
+    redis_client: &RedisClient,
+    indexer_config: &IndexerConfig,
+) -> anyhow::Result<()> {
+    // TODO handle err no such key
+    redis_client
+        .rename(
+            indexer_config.get_historical_redis_stream(),
+            indexer_config.get_redis_stream(),
+        )
+        .await?;
+
+    loop {
+        let stream_ids = redis_client
+            .xread(indexer_config.get_real_time_redis_stream(), 0, 100)
+            .await?;
+
+        if stream_ids.is_empty() {
+            break;
+        }
+
+        for stream_id in stream_ids {
+            let fields: Vec<(_, _)> = stream_id
+                .map
+                .into_iter()
+                .filter_map(|field| {
+                    if let ::redis::Value::Data(data) = field.1 {
+                        return Some((field.0, String::from_utf8(data).unwrap()));
+                    }
+
+                    // TODO data should always be serializable as string - log some
+                    // warning?
+                    None
+                })
+                .collect();
+
             redis_client
-                .srem(
-                    RedisClient::STREAMS_SET,
-                    indexer_config.get_real_time_redis_stream(),
-                )
+                .xadd(indexer_config.get_redis_stream(), &fields)
                 .await?;
             redis_client
-                .srem(
-                    RedisClient::STREAMS_SET,
-                    indexer_config.get_historical_redis_stream(),
-                )
-                .await?;
-
-            executors_handler
-                .stop(indexer_config.get_real_time_redis_stream())
-                .await?;
-            executors_handler
-                .stop(indexer_config.get_historical_redis_stream())
-                .await?;
-
-            // TODO handle err no such key
-            redis_client
-                .rename(
-                    indexer_config.get_historical_redis_stream(),
-                    indexer_config.get_redis_stream(),
-                )
-                .await?;
-
-            loop {
-                let stream_ids = redis_client
-                    .xread(indexer_config.get_real_time_redis_stream(), 0, 100)
-                    .await?;
-
-                if stream_ids.is_empty() {
-                    break;
-                }
-
-                for stream_id in stream_ids {
-                    let fields: Vec<(_, _)> = stream_id
-                        .map
-                        .into_iter()
-                        .filter_map(|field| {
-                            if let ::redis::Value::Data(data) = field.1 {
-                                return Some((field.0, String::from_utf8(data).unwrap()));
-                            }
-
-                            // TODO data should always be serializable as string - log some
-                            // warning?
-                            None
-                        })
-                        .collect();
-
-                    redis_client
-                        .xadd(indexer_config.get_redis_stream(), &fields)
-                        .await?;
-                    redis_client
-                        .xdel(indexer_config.get_real_time_redis_stream(), stream_id.id)
-                        .await?
-                }
-            }
+                .xdel(indexer_config.get_real_time_redis_stream(), stream_id.id)
+                .await?
         }
     }
 
