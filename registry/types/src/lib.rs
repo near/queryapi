c4188b69dc51ce93e1db1dfc6048b9d6d6ae5c5b
Morgan Mccauley <morgan@mccauley.co.nz>
2024-02-05 14:47:07 +1300

    refactor: Remove unnused fields from registry types

--- registry/types/src/lib.rs
+++ registry/types/src/lib.rs
@@ -17,14 +17,6 @@ use serde::{Deserialize, Serialize};
 type FunctionName = String;
 
 #[derive(Clone, Debug, Serialize, Deserialize, BorshSerialize, BorshDeserialize, PartialEq, Eq)]
-#[serde(rename_all = "SCREAMING_SNAKE_CASE")]
-pub enum Status {
-    Any,
-    Success,
-    Fail,
-}
-
-#[derive(Clone, Debug, Serialize, Deserialize, BorshSerialize, BorshDeserialize, PartialEq, Eq)]
 #[serde(tag = "rule", rename_all = "SCREAMING_SNAKE_CASE")]
 pub enum MatchingRule {
     ActionAny {
@@ -45,6 +37,7 @@ pub enum MatchingRule {
 }
 
 #[derive(Clone, Debug, Serialize, Deserialize, BorshSerialize, BorshDeserialize, PartialEq, Eq)]
+#[serde(tag = "kind", rename_all = "SCREAMING_SNAKE_CASE")]
 pub enum IndexerRuleKind {
     Action,
     Event,
@@ -53,14 +46,54 @@ pub enum IndexerRuleKind {
 }
 
 #[derive(Clone, Debug, Serialize, Deserialize, BorshSerialize, BorshDeserialize, PartialEq, Eq)]
-pub struct IndexerRule {
+pub struct OldIndexerRule {
     pub indexer_rule_kind: IndexerRuleKind,
     pub matching_rule: MatchingRule,
+    // These are not set, and not used anywhere
     pub id: Option<u32>,
     pub name: Option<String>,
 }
 
 #[derive(BorshSerialize, BorshDeserialize, Serialize, Deserialize, Clone, Debug, PartialEq, Eq)]
+pub struct OldIndexerConfig {
+    pub code: String,
+    pub start_block_height: Option<u64>,
+    pub schema: Option<String>,
+    pub filter: OldIndexerRule,
+    pub updated_at_block_height: Option<u64>,
+    pub created_at_block_height: u64,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize, BorshSerialize, BorshDeserialize, PartialEq, Eq)]
+#[serde(rename_all = "SCREAMING_SNAKE_CASE")]
+pub enum Status {
+    Any,
+    Success,
+    Fail,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize, BorshSerialize, BorshDeserialize, PartialEq, Eq)]
+#[serde(tag = "kind", rename_all = "SCREAMING_SNAKE_CASE")]
+pub enum IndexerRule {
+    ActionAny {
+        affected_account_id: String,
+        status: Status,
+    },
+    ActionFunctionCall {
+        affected_account_id: String,
+        status: Status,
+        function: String,
+    },
+    Event {
+        contract_account_id: String,
+        standard: String,
+        version: String,
+        event: String,
+    },
+}
+
+#[derive(BorshSerialize, BorshDeserialize, Serialize, Deserialize, Clone, Debug, PartialEq, Eq)]
+#[serde(rename_all = "SCREAMING_SNAKE_CASE")]
 pub enum StartBlock {
     /// Specific block height to start indexing from
     Height(u64),
@@ -73,9 +106,8 @@ pub enum StartBlock {
 #[derive(BorshSerialize, BorshDeserialize, Serialize, Deserialize, Clone, Debug, PartialEq, Eq)]
 pub struct IndexerConfig {
     pub code: String,
-    pub start_block_height: Option<u64>,
     pub start_block: StartBlock,
-    pub schema: Option<String>,
+    pub schema: String,
     pub filter: IndexerRule,
     pub updated_at_block_height: Option<u64>,
     pub created_at_block_height: u64,
@@ -83,6 +115,6 @@ pub struct IndexerConfig {
 
 #[derive(Debug, Serialize, Deserialize, PartialEq, Eq)]
 pub enum AccountOrAllIndexers {
-    All(HashMap<AccountId, HashMap<FunctionName, IndexerConfig>>),
-    Account(HashMap<FunctionName, IndexerConfig>),
+    All(HashMap<AccountId, HashMap<FunctionName, OldIndexerConfig>>),
+    Account(HashMap<FunctionName, OldIndexerConfig>),
 }
