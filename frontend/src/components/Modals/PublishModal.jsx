5d4cea6bfcbcef38ec592ded79f8dcfc211405c6
kevin <kevin101zhang@gmail.com>
2024-02-16 16:46:16 -0500

    fix: removed console.log

--- frontend/src/components/Modals/PublishModal.jsx
+++ frontend/src/components/Modals/PublishModal.jsx
@@ -31,7 +31,6 @@ export const PublishModal = ({
     }
 
     if (!validateContractIds(indexerConfig.filter)) {
-      console.log("Invalid contract name",indexerConfig.filter )
       setError( () => "Please provide a valid contract name")
       return
     }
