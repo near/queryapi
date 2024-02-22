b856d8224add20ef634a78e1b8a16e75fcaf5bca
Morgan Mccauley <morgan@mccauley.co.nz>
2024-02-23 09:59:50 +1300

    chore: Remove legacy code

--- indexer/download_test_blocks.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-mkdir -p ./blocks
-
-# Iterate over all script arguments
-for block_id in "$@"
-do
-  curl -o "./blocks/${block_id}.json" "https://70jshyr5cb.execute-api.eu-central-1.amazonaws.com/block/${block_id}?snake_case=true"
-done
