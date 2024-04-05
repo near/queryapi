f4c7ccb7a1a1c05a22f3e0cc8858ead542b8741c
Kevin Zhang <42101107+Kevin101Zhang@users.noreply.github.com>
2024-04-05 14:49:19 -0400

    Update log-entry.test.ts
    removed useless test

--- runner/src/log-entry/log-entry.test.ts
+++ runner/src/log-entry/log-entry.test.ts
@@ -10,11 +10,6 @@ describe('LogEntry', () => {
     expect(timestampDifference).toBeLessThanOrEqual(1000);
   });
 
-  test('should create a LogEntry instance with block height', () => {
-    const logEntry = new LogEntry('Test message', LogLevel.INFO, LogType.SYSTEM, 12345);
-    expect(logEntry.blockHeight).toBe(12345);
-  });
-
   test('systemInfo static method should create a LogEntry instance with predefined parameters', () => {
     const systemLogEntry = LogEntry.systemInfo('System info message', 67890);
     expect(systemLogEntry.type).toBe(LogType.SYSTEM);
