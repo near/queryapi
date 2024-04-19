c2f5f0d7c567c911ebb043d677eb353fd24616a1
Darun Seethammagari <darun@near.org>
2024-04-19 15:20:49 -0700

    fix: Continue Replacement of Logs/Metadata tables after untracking failures (#675)
    Errors in provisioning should be logged to the machine as they can
    potentially be overwritten by errors in the finally block of the parent
    try catch.
    
    We ideally want to move the provisioning out to its own try catch but
    this is a simple fix for the time being.
    
    In addition the PRs to replace the logs/metadata tables failed due to
    untracking being partially successful. This PR allows untracking errors.

--- runner/src/provisioner/provisioner.ts
+++ runner/src/provisioner/provisioner.ts
@@ -263,9 +263,25 @@ export default class Provisioner {
     await wrapError(
       async () => {
         const tableNames = await this.getTableNames(indexerConfig.schemaName(), indexerConfig.databaseName());
-        const tablesToDelete: string[] = tableNames.filter((tableName: string) => tableName === oldLogsTable || tableName === oldMetadataTable);
-        if (tablesToDelete.length > 0) {
-          await this.hasuraClient.untrackTables(indexerConfig.databaseName(), indexerConfig.schemaName(), tablesToDelete, true);
+        if (tableNames.includes(oldLogsTable)) {
+          try {
+            await this.hasuraClient.untrackTables(indexerConfig.databaseName(), indexerConfig.schemaName(), [oldLogsTable], true);
+          } catch (err) {
+            const error = err as Error;
+            if (!error.message.includes('already untracked')) {
+              console.error(error.message);
+            }
+          }
+        }
+        if (tableNames.includes(oldMetadataTable)) {
+          try {
+            await this.hasuraClient.untrackTables(indexerConfig.databaseName(), indexerConfig.schemaName(), [oldMetadataTable], true);
+          } catch (err) {
+            const error = err as Error;
+            if (!error.message.includes('already untracked')) {
+              console.error(error.message);
+            }
+          }
         }
         if (tableNames.includes(oldLogsTable)) {
           await this.hasuraClient.executeSqlOnSchema(indexerConfig.databaseName(), indexerConfig.schemaName(), `DROP TABLE IF EXISTS ${oldLogsTable} CASCADE;`);
