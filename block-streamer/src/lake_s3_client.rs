abd68a13e2d2f95ffa54a53c6f77f12682c29aba
Morgan Mccauley <morgan@mccauley.co.nz>
2024-04-30 13:35:56 +1200

    tracing

--- block-streamer/src/lake_s3_client.rs
+++ block-streamer/src/lake_s3_client.rs
@@ -10,8 +10,9 @@ use futures::future::Shared;
 use futures::{Future, FutureExt};
 use near_lake_framework::s3_client::{GetObjectBytesError, ListCommonPrefixesError};
 use std::collections::HashMap;
-// use tokio::sync::Mutex;
-use std::sync::Mutex;
+use tokio::sync::Mutex;
+use tracing::{instrument, trace, Instrument};
+// use std::sync::Mutex;
 
 use crate::metrics;
 
@@ -53,11 +54,9 @@ impl SharedLakeS3ClientImpl {
 
 #[async_trait]
 impl near_lake_framework::s3_client::S3Client for SharedLakeS3ClientImpl {
+    #[instrument(skip(self), level = "trace")]
     async fn get_object_bytes(&self, bucket: &str, prefix: &str) -> GetObjectBytesResult {
-        let timer = metrics::LAKE_CACHE_LOCK_WAIT_SECONDS.start_timer();
         let bytes = self.inner.get_object_bytes_cached(bucket, prefix).await;
-        let duration = timer.stop_and_record();
-        // eprintln!("duration = {:#?}", duration);
 
         bytes
     }
@@ -87,10 +86,24 @@ impl FuturesCache {
         for _ in 0..shard_count {
             shards.push(Mutex::new(HashMap::new()));
         }
+
         Self {
             shards,
             shard_count,
         }
+    }
+
+    #[instrument(skip_all, level = "trace")]
+    async fn lock_shard(
+        &self,
+        key: &str,
+    ) -> tokio::sync::MutexGuard<'_, HashMap<String, SharedGetObjectBytesFuture>> {
+        let timer = metrics::LAKE_CACHE_LOCK_WAIT_SECONDS.start_timer();
+        let shard_index = self.determine_shard(key);
+        let lock = self.shards[shard_index].lock().await;
+        let duration = timer.stop_and_record();
+        eprintln!("duration = {:#?}", duration);
+        lock
     }
 
     async fn get_or_set_with(
@@ -98,13 +111,8 @@ impl FuturesCache {
         key: String,
         f: impl FnOnce() -> SharedGetObjectBytesFuture,
     ) -> SharedGetObjectBytesFuture {
-        let shard_index = self.determine_shard(&key);
-
-        let timer = metrics::LAKE_CACHE_LOCK_WAIT_SECONDS.start_timer();
-        let mut shard = self.shards[shard_index].lock().unwrap();
-        let duration = timer.stop_and_record();
-        // eprintln!("lock duration = {:#?}, key = {:#?}", duration, key);
-
+        let mut shard = self.lock_shard(&key).await;
+        // TODO: Needs to be atmoic
         match shard.get(&key) {
             Some(future) => future.clone(),
             None => {
@@ -116,8 +124,7 @@ impl FuturesCache {
     }
 
     async fn remove(&self, key: &str) {
-        let shard_index = self.determine_shard(key);
-        let mut shard = self.shards[shard_index].lock().unwrap();
+        let mut shard = self.lock_shard(key).await;
         shard.remove(key);
     }
 
@@ -164,21 +171,29 @@ impl LakeS3Client {
 
             Ok(bytes)
         }
+        .instrument(tracing::trace_span!("get_object_bytes_shared"))
         .boxed()
         .shared()
     }
 
+    #[instrument(skip_all, level = "trace")]
     async fn get_object_bytes_cached(&self, bucket: &str, prefix: &str) -> GetObjectBytesResult {
         let get_object_bytes_future = self
             .futures_cache
             .get_or_set_with(prefix.to_string(), || {
+                trace!("creating future");
                 self.get_object_bytes_shared(bucket, prefix)
             })
             .await;
 
+        trace!("read from cache");
+
         let get_object_bytes_result = get_object_bytes_future.await;
 
+        trace!("awaited future");
+
         if get_object_bytes_result.is_err() {
+            trace!("removing failed future");
             self.futures_cache.remove(prefix).await;
         }
 
@@ -245,15 +260,39 @@ mod tests {
     use std::sync::atomic::{AtomicUsize, Ordering};
     use std::sync::Barrier;
 
-    use aws_sdk_s3::error::SdkError;
-    use aws_sdk_s3::operation::get_object::GetObjectError;
     use aws_sdk_s3::operation::get_object::GetObjectOutput;
-    use aws_sdk_s3::types::error::NoSuchKey;
     use near_lake_framework::s3_client::S3Client;
 
-    // #[ignore]
+    use std::time::Instant;
+    use tracing_subscriber::prelude::*;
+    use tracing_timing::{Builder, Histogram};
+
+    #[ignore]
     #[tokio::test]
     async fn deduplicates_parallel_requests() {
+        // console_subscriber::init();
+
+        let fmt_layer = tracing_subscriber::fmt::layer().compact().with_span_events(
+            tracing_subscriber::fmt::format::FmtSpan::EXIT
+                | tracing_subscriber::fmt::format::FmtSpan::ENTER,
+        );
+
+        // Set up the timing layer
+        let timing_layer =
+            Builder::default().layer(|| Histogram::<u64>::new_with_max(10_000_000, 2).unwrap());
+
+        // Combine the layers
+        let subscriber = tracing_subscriber::registry()
+            .with(tracing_subscriber::EnvFilter::from_default_env())
+            .with(fmt_layer)
+            .with(timing_layer);
+
+        // Initialize the subscriber
+        tracing::subscriber::set_global_default(subscriber)
+            .expect("setting default subscriber failed");
+
+        tracing::info!("deduplicates_parallel_requests");
+
         let s3_get_call_count = Arc::new(AtomicUsize::new(0));
 
         let call_count_clone = s3_get_call_count.clone();
@@ -278,37 +317,52 @@ mod tests {
 
         let shared_lake_s3_client = SharedLakeS3ClientImpl::new(LakeS3Client::new(s3_client));
 
-        let thread_count = 50;
+        use tracing::*;
+        use tracing_timing::{Builder, Histogram};
+
+        let subscriber =
+            Builder::default().build(|| Histogram::new_with_max(1_000_000, 2).unwrap());
+        let sid = subscriber.downcaster();
+
+        use tracing::Dispatch;
+
+        let dispatcher = Dispatch::new(subscriber);
+        let d2 = dispatcher.clone();
+
+        let thread_count = 1;
         let barrier = Arc::new(Barrier::new(thread_count));
         let handles: Vec<_> = (0..thread_count)
             .map(|_| {
                 let client = shared_lake_s3_client.clone();
+                let d2 = dispatcher.clone();
 
                 std::thread::spawn(move || {
                     let rt = tokio::runtime::Runtime::new().unwrap();
 
-                    rt.block_on(async {
-                        let start_block_height = 117500034;
-                        let lake_config = near_lake_framework::LakeConfigBuilder::default()
-                            .mainnet()
-                            .s3_client(client)
-                            .start_block_height(start_block_height)
-                            // .blocks_preload_pool_size(10)
-                            .build()
-                            .unwrap();
+                    dispatcher::with_default(&d2, || {
+                        rt.block_on(async {
+                            let start_block_height = 117500034;
+                            let lake_config = near_lake_framework::LakeConfigBuilder::default()
+                                .mainnet()
+                                .s3_client(client)
+                                .start_block_height(start_block_height)
+                                .blocks_preload_pool_size(10)
+                                .build()
+                                .unwrap();
 
-                        let (sender, mut stream) = near_lake_framework::streamer(lake_config);
+                            let (sender, mut stream) = near_lake_framework::streamer(lake_config);
 
-                        while let Some(streamer_message) = stream.recv().await {
-                            let block_height = streamer_message.block.header.height;
-                            if block_height == start_block_height + 100 {
-                                break;
+                            while let Some(streamer_message) = stream.recv().await {
+                                let block_height = streamer_message.block.header.height;
+                                if block_height == start_block_height + 9 {
+                                    break;
+                                }
+                                eprintln!("block_height = {:#?}", block_height);
                             }
-                            eprintln!("block_height = {:#?}", block_height);
-                        }
 
-                        drop(sender);
-                    })
+                            drop(sender);
+                        })
+                    });
                 })
             })
             .collect();
@@ -316,6 +370,90 @@ mod tests {
         for handle in handles {
             let _ = handle.join();
         }
+
+        fn break_once<I, F>(it: I, mut f: F) -> impl Iterator<Item = I::Item>
+        where
+            I: IntoIterator,
+            F: FnMut(&I::Item) -> bool,
+        {
+            let mut got_true = false;
+            it.into_iter().take_while(move |i| {
+                if got_true {
+                    // we've already yielded when f was true
+                    return false;
+                }
+                if f(i) {
+                    // this must be the first time f returns true
+                    // we should yield i, and then no more
+                    got_true = true;
+                }
+                // f returned false, so we should keep yielding
+                true
+            })
+        }
+
+        sid.downcast(&dispatcher).unwrap().with_histograms(|hs| {
+            assert_eq!(hs.len(), 1);
+            println!("{:?}", hs.keys());
+
+            let hs = &mut hs.get_mut("request").unwrap();
+            assert_eq!(hs.len(), 2);
+
+            hs.get_mut("fast").unwrap().refresh();
+            hs.get_mut("slow").unwrap().refresh();
+
+            println!("fast:");
+            let h = &hs["fast"];
+            println!(
+                "mean: {:.1}µs, p50: {}µs, p90: {}µs, p99: {}µs, p999: {}µs, max: {}µs",
+                h.mean() / 1000.0,
+                h.value_at_quantile(0.5) / 1_000,
+                h.value_at_quantile(0.9) / 1_000,
+                h.value_at_quantile(0.99) / 1_000,
+                h.value_at_quantile(0.999) / 1_000,
+                h.max() / 1_000,
+            );
+            for v in break_once(
+                h.iter_linear(25_000).skip_while(|v| v.quantile() < 0.01),
+                |v| v.quantile() > 0.95,
+            ) {
+                println!(
+                    "{:4}µs | {:40} | {:4.1}th %-ile",
+                    (v.value_iterated_to() + 1) / 1_000,
+                    "*".repeat(
+                        (v.count_since_last_iteration() as f64 * 40.0 / h.len() as f64).ceil()
+                            as usize
+                    ),
+                    v.percentile(),
+                );
+            }
+
+            println!("\nslow:");
+            let h = &hs["slow"];
+            println!(
+                "mean: {:.1}µs, p50: {}µs, p90: {}µs, p99: {}µs, p999: {}µs, max: {}µs",
+                h.mean() / 1000.0,
+                h.value_at_quantile(0.5) / 1_000,
+                h.value_at_quantile(0.9) / 1_000,
+                h.value_at_quantile(0.99) / 1_000,
+                h.value_at_quantile(0.999) / 1_000,
+                h.max() / 1_000,
+            );
+            for v in break_once(
+                h.iter_linear(25_000).skip_while(|v| v.quantile() < 0.01),
+                |v| v.quantile() > 0.95,
+            ) {
+                println!(
+                    "{:4}µs | {:40} | {:4.1}th %-ile",
+                    (v.value_iterated_to() + 1) / 1_000,
+                    "*".repeat(
+                        (v.count_since_last_iteration() as f64 * 40.0 / h.len() as f64).ceil()
+                            as usize
+                    ),
+                    v.percentile(),
+                );
+            }
+        });
 
         // for shard in shared_lake_s3_client.inner.futures_cache.shards.iter() {
         //     let lock = shard.lock().await;
@@ -325,9 +463,21 @@ mod tests {
         assert_eq!(s3_get_call_count.load(Ordering::SeqCst), 1);
     }
 
-    #[ignore]
+    // #[ignore]
     #[tokio::test]
     async fn temp() {
+        tracing_subscriber::registry()
+            .with(tracing_subscriber::EnvFilter::from_default_env())
+            .with(
+                tracing_subscriber::fmt::layer()
+                    .with_span_events(
+                        tracing_subscriber::fmt::format::FmtSpan::ENTER
+                            | tracing_subscriber::fmt::format::FmtSpan::EXIT,
+                    )
+                    .compact(),
+            )
+            .init();
+
         let aws_config = aws_config::from_env().load().await;
         let s3_config = aws_sdk_s3::Config::from(&aws_config);
         let s3_client = crate::s3_client::S3ClientImpl::new(s3_config);
@@ -346,7 +496,7 @@ mod tests {
                     let rt = tokio::runtime::Runtime::new().unwrap();
 
                     rt.block_on(async {
-                        let handles: Vec<_> = (117500034..117510034)
+                        let handles: Vec<_> = (117500034..117510134)
                             .map(|height| {
                                 let client = client.clone();
 
@@ -370,7 +520,7 @@ mod tests {
             })
             .collect();
 
-        println!("done");
+        println!("{:?}", handles);
 
         for handle in handles {
             let _ = handle.join();
