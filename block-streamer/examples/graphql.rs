9c96ab359f5731e6b5e1bd2160827189e67b7ff2
Darun Seethammagari <darunrs@gmail.com>
2024-05-20 12:16:56 -0700

    Fix static file path and convert example to async

--- block-streamer/examples/graphql.rs
+++ block-streamer/examples/graphql.rs
@@ -1,6 +1,6 @@
 use graphql_client::{GraphQLQuery, Response};
 use std::error::Error;
-use ::reqwest::blocking::Client;
+use ::reqwest::Client;
 
 #[allow(clippy::upper_case_acronyms)]
 type URI = String;
@@ -8,30 +8,31 @@ type Date = String;
 
 #[derive(GraphQLQuery)]
 #[graphql(
-    schema_path = "graphql/bitmap/schema.json",
-    query_path = "graphql/bitmap/query.graphql",
+    schema_path = "graphql/darunrs_near/schema.json",
+    query_path = "graphql/darunrs_near/get_bitmap_by_date.graphql",
     response_derives = "Debug",
     normalization = "rust"
 )]
 struct GetBitmapByDate;
 
-fn perform_my_query(variables: get_bitmap_by_date::Variables) -> Result<(), Box<dyn Error>> {
+async fn perform_my_query(variables: get_bitmap_by_date::Variables) -> Result<(), Box<dyn Error>> {
 
     // this is the important line
     let request_body = GetBitmapByDate::build_query(variables);
 
-    let client = reqwest::blocking::Client::new();
-    let res = client.post("https://queryapi-hasura-graphql-mainnet-vcqilefdcq-ew.a.run.app/v1/graphql").header("x-hasura-role", "darunrs_near").json(&request_body).send()?;
-    let response_body: Response<get_bitmap_by_date::ResponseData> = res.json()?;
+    let client = reqwest::Client::new();
+    let res = client.post("https://queryapi-hasura-graphql-mainnet-vcqilefdcq-ew.a.run.app/v1/graphql").header("x-hasura-role", "darunrs_near").json(&request_body).send().await?;
+    let response_body: Response<get_bitmap_by_date::ResponseData> = res.json().await?;
     println!("{:#?}", response_body);
     Ok(())
 }
 
-fn main() -> Result<(), Box<dyn Error>> {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
     let variables = get_bitmap_by_date::Variables {
         block_date: Some("2024-03-21".to_string()),
         limit: Some(100),
         offset: Some(0),
     };
-    perform_my_query(variables)
+    perform_my_query(variables).await
 }
