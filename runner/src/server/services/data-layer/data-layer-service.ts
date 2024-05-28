580810677cf1c884a3d6896a6486e7eada88fc5f
Morgan Mccauley <morgan@mccauley.co.nz>
2024-05-29 10:38:13 +1200

    feat: Trigger provisioning tasks asynchronously

--- runner/src/server/services/data-layer/data-layer-service.ts
+++ runner/src/server/services/data-layer/data-layer-service.ts
@@ -1,4 +1,4 @@
-import { type ServerUnaryCall, type sendUnaryData } from '@grpc/grpc-js';
+import { type ServerUnaryCall, type sendUnaryData, status, StatusBuilder } from '@grpc/grpc-js';
 
 import Provisioner from '../../../provisioner';
 import IndexerConfig from '../../../indexer-config';
@@ -9,18 +9,59 @@ import { type ProvisionRequest__Output } from '../../../generated/ProvisionReque
 import { type ProvisionResponse } from '../../../generated/ProvisionResponse';
 import { ProvisioningStatus } from '../../../generated/ProvisioningStatus';
 
+class ProvisioningTask {
+  public failed: boolean;
+  public pending: boolean;
+  public completed: boolean;
+
+  constructor (public readonly promise: Promise<void>) {
+    promise.then(() => {
+      this.completed = true;
+    }).catch((error) => {
+      this.failed = true;
+      return error;
+    }).finally(() => {
+      this.pending = false;
+    });
+
+    this.failed = false;
+    this.pending = true;
+    this.completed = false;
+  }
+}
+
+type ProvisioningTasks = Record<string, ProvisioningTask>;
+
 export function createDataLayerService (
-  provisioner: Provisioner = new Provisioner()
+  provisioner: Provisioner = new Provisioner(),
+  tasks: ProvisioningTasks = {}
 ): DataLayerHandlers {
   return {
-    CheckProvisioningStatus (_call: ServerUnaryCall<CheckProvisioningStatusRequest__Output, ProvisionResponse>, callback: sendUnaryData<ProvisionResponse>): void {
-      console.log('CheckProvisioningStatus called');
+    CheckProvisioningStatus (call: ServerUnaryCall<CheckProvisioningStatusRequest__Output, ProvisionResponse>, callback: sendUnaryData<ProvisionResponse>): void {
+      const { accountId, functionName } = call.request;
+      const task = tasks[`${accountId}/${functionName}`];
+
+      if (!task) {
+        const notFound = new StatusBuilder().withCode(status.NOT_FOUND).withDetails('Provisioning task does not exist').build();
+        callback(notFound);
+        return;
+      }
+
+      if (task.completed) {
+        callback(null, { status: ProvisioningStatus.COMPLETE });
+        return;
+      }
+
+      if (task.failed) {
+        callback(null, { status: ProvisioningStatus.FAILED });
+        return;
+      }
 
       callback(null, { status: ProvisioningStatus.PENDING });
     },
 
     Provision (call: ServerUnaryCall<ProvisionRequest__Output, ProvisionResponse>, callback: sendUnaryData<ProvisionResponse>): void {
-      console.log('Provision called');
+      // TODO exit if already provisioned
 
       const indexerConfig = new IndexerConfig(
         'redisStreamKey',
@@ -32,11 +73,9 @@ export function createDataLayerService (
         5
       );
 
-      provisioner.provisionUserApi(indexerConfig).then(() => {
-        callback(null, { status: ProvisioningStatus.COMPLETE });
-      }).catch((error) => {
-        callback(error, null);
-      });
+      tasks[indexerConfig.fullName()] = new ProvisioningTask(provisioner.provisionUserApi(indexerConfig));
+
+      callback(null, { status: ProvisioningStatus.PENDING });
     }
   };
 }
