ac352bbcbb7722b06b517fb0ae31786ada4d231e
Kevin Zhang <42101107+Kevin101Zhang@users.noreply.github.com>
2024-05-28 17:19:26 -0400

    Fix dead Links. (#750)
    The new website for [near.org](http://near.org/) is simply a marketing
    website now served by this repo and will no longer serve as the gatway:
    https://github.com/near/nearorg
    The gateway now lives at [dev.near.org](http://dev.near.org/) and
    remains the same: https://github.com/near/near-discovery
    
    Looked for dead links in queryAPI repo. A few I avoided that used
    alpha.near have not been modified.
    Changed most near.org to dev.near.org

--- frontend/widgets/src/QueryApi.Dashboard.jsx
+++ frontend/widgets/src/QueryApi.Dashboard.jsx
@@ -324,8 +324,8 @@ const selectIndexerPage = (viewName) => {
   });
 };
 const indexerView = (accountId, indexerName) => {
-  const editUrl = `https://near.org/#/${REPL_ACCOUNT_ID}/widget/QueryApi.App?selectedIndexerPath=${accountId}/${indexerName}`;
-  const statusUrl = `https://near.org/#/${REPL_ACCOUNT_ID}/widget/QueryApi.App?selectedIndexerPath=${accountId}/${indexerName}&view=indexer&activeIndexerView=status`;
+  const editUrl = `https://dev.near.org/#/${REPL_ACCOUNT_ID}/widget/QueryApi.App?selectedIndexerPath=${accountId}/${indexerName}`;
+  const statusUrl = `https://dev.near.org/#/${REPL_ACCOUNT_ID}/widget/QueryApi.App?selectedIndexerPath=${accountId}/${indexerName}&view=indexer&activeIndexerView=status`;
   const playgroundLink = `https://cloud.hasura.io/public/graphiql?endpoint=${REPL_GRAPHQL_ENDPOINT}/v1/graphql&header=x-hasura-role%3A${accountId.replaceAll(
     ".",
     "_"
@@ -406,7 +406,7 @@ return (
     <Main>
       <Section active={state.activeTab === "explore"}>
         <NavBarLogo
-          href={`https://near.org/#/${REPL_ACCOUNT_ID}/widget/QueryApi.App`}
+          href={`https://dev.near.org/#/${REPL_ACCOUNT_ID}/widget/QueryApi.App`}
           title="QueryApi"
           onClick={() => selectTab("explore")}
         >
