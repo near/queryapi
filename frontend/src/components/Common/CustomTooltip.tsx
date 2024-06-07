bf2f3ff700c203671755d66a5efd52e76822b017
kevin <kevin101zhang@gmail.com>
2024-06-07 13:42:35 -0400

    refactored Tooltip

--- frontend/src/components/Common/CustomTooltip.tsx
+++ frontend/src/components/Common/CustomTooltip.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import { Tooltip, OverlayTriggerProps, OverlayTrigger } from 'react-bootstrap';
+import React, { useState } from 'react';
 
 export enum TooltipDirection {
     Top = 'top',
@@ -14,24 +13,51 @@ interface CustomTooltipProps {
     children: React.ReactElement<any>;
 }
 
-const CustomTooltip: React.FC<CustomTooltipProps> = ({ message, direction = TooltipDirection.Top, children }) => {
-    const renderTooltip = (props: OverlayTriggerProps) => (
-        <Tooltip id="custom-tooltip" {...props}
-            className="text-xs rounded shadow-sm">
-            {message}
-        </Tooltip>
-    );
 
-    const overlay = React.useMemo(() => renderTooltip({} as OverlayTriggerProps), [message]);
+const CustomTooltip: React.FC<CustomTooltipProps> = ({ message, direction = TooltipDirection.Top, children }) => {
+    const [visible, setVisible] = useState(false);
+
+    const showTooltip = () => setVisible(true);
+    const hideTooltip = () => setVisible(false);
+
+    const getTooltipPositionClass = (direction: TooltipDirection | undefined): string => {
+        switch (direction) {
+            case TooltipDirection.Top:
+                return 'bottom-full left-1/2 transform -translate-x-1/2';
+            case TooltipDirection.Bottom:
+                return 'top-full left-1/2 transform -translate-x-1/2';
+            case TooltipDirection.Left:
+                return 'top-1/2 right-full transform translate-y-1/2';
+            case TooltipDirection.Right:
+                return 'top-1/2 left-full transform translate-y-1/2';
+            default:
+                return '';
+        }
+    };
+
+    const tooltipClasses = [
+        getTooltipPositionClass(direction),
+        'tooltip-bubble',
+        'text-xxs',
+        'rounded',
+        'shadow-sm',
+        'bg-black',
+        'text-white',
+        'px-2',
+        'absolute',
+        'text-center',
+        'w-max',
+    ].filter(Boolean).join(' ');
 
     return (
-        <OverlayTrigger
-            placement={direction}
-            delay={{ show: 250, hide: 350 }}
-            overlay={overlay}
-        >
+        <div className="relative inline-block" onMouseEnter={showTooltip} onMouseLeave={hideTooltip}>
             {children}
-        </OverlayTrigger>
+            {visible && (
+                <div className={tooltipClasses}>
+                    <div className="tooltip-message">{message}</div>
+                </div>
+            )}
+        </div>
     );
 };
 
