afdf2c14ef08dd1ba2f23319cd268c638235d2de
Morgan Mccauley <morgan@mccauley.co.nz>
2024-06-17 21:10:28 +1200

    fix: Update data layer grpc examples to match proto

--- runner-client/examples/start_provisioning_task.rs
+++ runner-client/examples/start_provisioning_task.rs
@@ -8,7 +8,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut client = DataLayerClient::connect("http://localhost:7001").await?;
 
     let response = client
-        .provision(Request::new(ProvisionRequest {
+        .start_provisioning_task(Request::new(ProvisionRequest {
             account_id: "morgs.near".to_string(),
             function_name: "test2".to_string(),
             schema: "create table blocks();".to_string(),
