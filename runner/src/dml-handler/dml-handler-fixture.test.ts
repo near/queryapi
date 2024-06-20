40a209c97d734df902a3cd6ebf3f21295fbe2b07
Darun Seethammagari <darunrs@gmail.com>
2024-06-20 17:59:40 +0530

    refactor of data storage

--- runner/src/dml-handler/dml-handler-fixture.test.ts
+++ runner/src/dml-handler/dml-handler-fixture.test.ts
@@ -1,23 +1,28 @@
 import { TableDefinitionNames } from "../indexer";
-import DmlHandlerFixture from "./dml-handler-fixture";
+import InMemoryDmlHandler from "./dml-handler-fixture";
 
 describe('DML Handler Fixture Tests', () => {
+  const SIMPLE_SCHEMA = `CREATE TABLE
+    "posts" (
+      "id" SERIAL NOT NULL,
+      "account_id" VARCHAR NOT NULL,
+      "block_height" DECIMAL(58, 0) NOT NULL,
+      "receipt_id" VARCHAR NOT NULL,
+      "content" TEXT NOT NULL,
+      "block_timestamp" DECIMAL(20, 0) NOT NULL,
+      "accounts_liked" JSONB NOT NULL DEFAULT '[]',
+      "last_comment_timestamp" DECIMAL(20, 0),
+      CONSTRAINT "posts_pkey" PRIMARY KEY ("id", "account_id")
+    );`;
   let TABLE_DEFINITION_NAMES: TableDefinitionNames = {
-    originalTableName: '"test_table"',
-    originalColumnNames: new Map<string, string>([
-      ['account_id', 'account_id'],
-      ['block_height', '"block_height"'],
-      ['block_timestamp', 'block_timestamp'],
-      ['content', '"content"'],
-      ['receipt_id', 'receipt_id'],
-      ['accounts_liked', '"accounts_liked"']
-    ])
+    originalTableName: 'posts',
+    originalColumnNames: new Map<string, string>([])
   };
 
-  let dmlHandlerFixture: DmlHandlerFixture;
+  let dmlHandler: InMemoryDmlHandler;
 
   beforeEach(() => {
-    dmlHandlerFixture = new DmlHandlerFixture();
+    dmlHandler = new InMemoryDmlHandler(SIMPLE_SCHEMA);
   });
 
   test('insert two rows', async () => {
@@ -25,14 +30,43 @@ describe('DML Handler Fixture Tests', () => {
       account_id: 'morgs_near',
       block_height: 1,
       receipt_id: 'abc',
+      content: "some content",
+      block_timestamp: 123,
+      accounts_liked: [],
+      last_comment_timestamp: 456
     },
     {
       account_id: 'morgs_near',
       block_height: 2,
       receipt_id: 'abc',
+      content: "some content",
+      block_timestamp: 123,
+      accounts_liked: [],
+      last_comment_timestamp: 456
     }];
 
-    const result = await dmlHandlerFixture.insert(TABLE_DEFINITION_NAMES, inputObj);
-    expect(result).toEqual(inputObj);
+    const correctResult = [{
+      id: 0,
+      account_id: 'morgs_near',
+      block_height: 1,
+      receipt_id: 'abc',
+      content: "some content",
+      block_timestamp: 123,
+      accounts_liked: [],
+      last_comment_timestamp: 456
+    },
+    {
+      id: 1,
+      account_id: 'morgs_near',
+      block_height: 2,
+      receipt_id: 'abc',
+      content: "some content",
+      block_timestamp: 123,
+      accounts_liked: [],
+      last_comment_timestamp: 456
+    }];
+
+    const result = await dmlHandler.insert(TABLE_DEFINITION_NAMES, inputObj);
+    expect(result).toEqual(correctResult);
   });
 });
