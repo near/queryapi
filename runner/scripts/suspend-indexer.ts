d0eeb55aa4b7e38c47fe453844ee7d17248974e9
Morgan Mccauley <morgan@mccauley.co.nz>
2024-06-24 11:52:22 +1200

    feat: Log suspension

--- runner/scripts/suspend-indexer.ts
+++ runner/scripts/suspend-indexer.ts
@@ -18,23 +18,46 @@ import * as fs from 'fs'
 import * as grpc from '@grpc/grpc-js'
 import * as protoLoader from '@grpc/proto-loader'
 
+import Provisioner from '../src/provisioner'
+import IndexerConfig from '../src/indexer-config'
+import IndexerMeta, { LogEntry } from '../src/indexer-meta';
+
 const COORDINATOR_PROTO_PATH = '../coordinator/proto/indexer_manager.proto';
 
 assert(exists(COORDINATOR_PROTO_PATH), 'Coordinator proto file not found. Make sure you run this script from the root directory.');
 assert(process.argv.length === 4, 'Usage: npm run script:suspend-indexer -- <accountId> <functionName>');
 assert(process.env.COORDINATOR_PORT, 'COORDINATOR_PORT env var is required');
+assert(process.env.HASURA_ADMIN_SECRET, 'HASURA_ADMIN_SECRET env var is required');
+assert(process.env.HASURA_ENDPOINT, 'HASURA_ENDPOINT env var is required');
+assert(process.env.PGPORT, 'PGPORT env var is required');
+assert(process.env.PGHOST, 'PGHOST env var is required');
 
 const [_binary, _file, accountId, functionName] = process.argv;
 const { COORDINATOR_PORT } = process.env;
 
+const provisioner = new Provisioner();
+
 main();
 
-function main() {
-  console.log(`Disabling indexer: ${accountId}/${functionName}\n`);
+async function main() {
+  const config = new IndexerConfig('redis stream', accountId, functionName, 0, 'code', 'schema', 2);
+
+  console.log(`Suspending indexer: ${accountId}/${functionName}\n`);
 
   const indexerManager = createIndexerManagerClient();
 
-  indexerManager.disable({ accountId: 'morgs.near', functionName: 'sqs' }, console.log);
+  indexerManager.disable({ accountId, functionName }, console.log);
+
+  console.log('Logging suspension notification');
+
+  const pgCredentials = await provisioner.getPostgresConnectionParameters(config.userName());
+  const meta = new IndexerMeta(config, pgCredentials);
+
+  await meta.writeLogs([
+    LogEntry.systemInfo('Suspending Indexer due to inactivity'),
+  ]);
+
+  console.log('Done')
 }
 
 function exists(path: string): boolean {
--- runner/src/indexer-meta/index.ts
+++ runner/src/indexer-meta/index.ts
@@ -1,2 +1,3 @@
 export { default } from './indexer-meta';
 export { IndexerStatus, METADATA_TABLE_UPSERT, MetadataFields } from './indexer-meta';
+export { default as LogEntry } from './log-entry';
