6ec4d291121dabedcf2d96c15203c1a88e60d6fb
kevin <kevin101zhang@gmail.com>
2024-07-02 14:55:35 -0400

    feat: refactor index.js

--- frontend/src/pages/query-api-editor/index.js
+++ frontend/src/pages/query-api-editor/index.js
@@ -11,34 +11,20 @@ const QueryApiEditorPage = ({ router }) => {
   const { setAccountId, setIndexerName, showLogsView } = useContext(IndexerDetailsContext);
 
   useEffect(() => {
-    if (accountId == undefined || indexerName == undefined) {
-      return;
-    }
+    if (!accountId || !indexerName) return;
     setAccountId(accountId);
     setIndexerName(indexerName);
-  }, [accountId, indexerName, setAccountId, setIndexerName]);
+  }, [accountId, indexerName]);
 
-  if (accountId == undefined || indexerName == undefined) {
+  if (!accountId || !indexerName) {
     return (
-      <>
-        <Alert className="px-3 pt-3" variant="info">
-          Both accountId and IndexerName need to be specified in the URL.
-        </Alert>
-      </>
+      <Alert className="px-3 pt-3" variant="info">
+        Both accountId and IndexerName need to be specified in the URL.
+      </Alert>
     );
   }
-  return (
-    <>
-      {showLogsView ? (
-        <IndexerLogsContainer />
-      ) : (
-        <Editor
-          actionButtonText="Publish"
-          onLoadErrorText="An error occurred while trying to query indexer function details from registry."
-        />
-      )}
-    </>
-  );
+  
+  return showLogsView ? <IndexerLogsContainer /> : <Editor />;
 };
 
 export default withRouter(QueryApiEditorPage);
