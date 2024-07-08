7523181b92f32a888929fe6e3e1bf598f27c1e53
kevin <kevin101zhang@gmail.com>
2024-07-08 16:20:59 -0400

    chore

--- frontend/src/pages/temp.ts
+++
@@ -1,9 +0,0 @@
-// <NearSocialBridgeProvider waitForStorage fallback = {< Spinner />}>
-//     <ApolloProvider client={ client }>
-//         <IndexerDetailsProvider>
-//         <ModalProvider>
-//         <Component { ...pageProps } />
-//         </ModalProvider>
-//         < /IndexerDetailsProvider>
-//         < /ApolloProvider>
-//         < /NearSocialBridgeProvider>
