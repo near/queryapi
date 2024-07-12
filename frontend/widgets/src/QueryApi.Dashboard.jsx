04f690c69c0d5c3385dc11d25dbe26ce7c7c2a68
kevin <kevin101zhang@gmail.com>
2024-07-12 15:13:36 -0400

    feat:refactor and removed dead code

--- frontend/widgets/src/QueryApi.Dashboard.jsx
+++ frontend/widgets/src/QueryApi.Dashboard.jsx
@@ -1,70 +1,10 @@
 const accountId = context.accountId;
-const [selected_accountId, selected_indexerName] = props.selectedIndexerPath ? props.selectedIndexerPath.split("/") : [undefined, undefined];
-const limit = 7;
 
 const [activeTab, setActiveTab] = useState(props.view === "create-new-indexer" ? "create-new-indexer" : props.selectedIndexerPath ? "indexer" : "explore");
 const [activeIndexerTabView, setActiveIndexerTabView] = useState(props.activeIndexerView ?? "editor");
-const [myIndexers, setMyIndexers] = useState([]);
+
 const [allIndexers, setAllIndexers] = useState([]);
-const [selectedIndexerName, setSelectedIndexerName] = useState('')
-const [checkedItems, setCheckedItems] = useState({});
-
-const CheckboxContainer = styled.div`
-  margin-bottom: 16px;
-`;
-
-const CheckboxLabel = styled.label`
-  display: block;
-  margin-bottom: 8px;
-`;
-
-const SubCheckboxContainer = styled.div`
-  margin-left: 24px;
-  margin-top: 8px;
-`;
-
-const Checkbox = styled.input`
-  margin-right: 8px;
-`;
-
-const handleParentChange = (methodName) => {
-  setCheckedItems((prevState) => {
-    const newState = { ...prevState };
-    const parentChecked = !prevState[methodName];
-
-    newState[methodName] = parentChecked;
-
-    // Check/uncheck immediate sub-checkboxes
-    if (parentChecked) {
-      checkboxData.forEach((item) => {
-        if (item.method_name === methodName && item.schema.properties) {
-          Object.keys(item.schema.properties).forEach((property) => {
-            newState[`${methodName}_${property}`] = true;
-          });
-        }
-      });
-    } else {
-      // Uncheck all sub-checkboxes if parent is unchecked
-      checkboxData.forEach((item) => {
-        if (item.method_name === methodName && item.schema.properties) {
-          Object.keys(item.schema.properties).forEach((property) => {
-            newState[`${methodName}_${property}`] = false;
-          });
-        }
-      });
-    }
-
-    return newState;
-  });
-};
-
-const handleChildChange = (parent, child) => {
-  setCheckedItems((prevState) => ({
-    ...prevState,
-    [`${parent}_${child}`]: !prevState[`${parent}_${child}`],
-  }));
-};
-
+const [checkboxState, setCheckboxState] = useState(initialCheckboxState);
 
 const checkBoxData = [
   {
@@ -105,25 +45,74 @@ const checkBoxData = [
   },
 ];
 
-
-Near.asyncView(`${REPL_REGISTRY_CONTRACT_ID}`, "list_all").then((data) => {
-  const indexers = [];
-  Object.keys(data).forEach((accountId) => {
-    Object.keys(data[accountId]).forEach((functionName) => {
-      indexers.push({
-        accountId: accountId,
-        indexerName: functionName,
+useEffect(() => {
+  Near.asyncView(`${REPL_REGISTRY_CONTRACT_ID}`, "list_all").then((data) => {
+    const indexers = [];
+    Object.keys(data).forEach((accountId) => {
+      Object.keys(data[accountId]).forEach((functionName) => {
+        indexers.push({
+          accountId: accountId,
+          indexerName: functionName,
+        });
       });
     });
+    setAllIndexers(indexers)
   });
+}, []);
 
-  let my_indexers = indexers.filter(
-    (indexer) => indexer.accountId === accountId
-  );
+const initialCheckboxState = checkBoxData.reduce((acc, item) => {
+  acc[item.method_name] = false;
+  if (item.schema.properties) {
+    Object.keys(item.schema.properties).forEach(property => {
+      acc[`${item.method_name}::${property}`] = false;
+    });
+  }
+  return acc;
+}, {});
 
-  setMyIndexers(my_indexers);
-  setAllIndexers(indexers)
-});
+const handleParentChange = (methodName) => {
+  const newState = { ...checkboxState };
+  const isChecked = !checkboxState[methodName];
+  newState[methodName] = isChecked;
+  checkBoxData.forEach(item => {
+    if (item.method_name === methodName && item.schema.properties) {
+      Object.keys(item.schema.properties).forEach(property => {
+        newState[`${methodName}::${property}`] = isChecked;
+      });
+    }
+  });
+  setCheckboxState(newState);
+};
+
+const handleChildChange = (childId) => {
+  setCheckboxState({
+    ...checkboxState,
+    [childId]: !checkboxState[childId],
+  });
+};
+
+const CheckboxContainer = styled.div`
+  margin-bottom: 10px;
+`;
+
+const CheckboxLabel = styled.label`
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+  font-size: 16px;
+  margin-bottom: 5px;
+`;
+
+const SubCheckboxContainer = styled.div`
+  margin-left: 20px;
+  border-left: 2px solid #ccc;
+  padding-left: 10px;
+`;
+
+const Checkbox = styled.input`
+  margin-right: 10px;
+  cursor: pointer;
+`;
 
 // TOP HALF
 const Hero = styled.div`
@@ -392,38 +381,11 @@ function CustomTable() {
 }
 
 // BELOW IS ORIGINAL STYLED COMPONENTS
-const Subheading = styled.h2`
-  display: block;
-  margin: 0;
-  font-size: 14px;
-  line-height: 20px;
-  color: ${(p) => (p.bold ? "#11181C !important" : "#687076 !important")};
-  font-weight: ${(p) => (p.bold ? "600" : "400")};
-  font-size: ${(p) => (p.small ? "12px" : "14px")};
-  overflow: ${(p) => (p.ellipsis ? "hidden" : "visible")};
-  text-overflow: ${(p) => (p.ellipsis ? "ellipsis" : "unset")};
-  white-space: nowrap;
-  outline: none;
-`;
-
-const Editor = styled.div`
-`;
-const Status = styled.div`
-`;
-
 const Wrapper = styled.div`
   margin-inline: 12px;
   margin-top: calc(var(--body-top-padding) * -1);
 `;
 
-const NavBarLogo = styled.a`
-  padding-top: 0.3125rem;
-  padding-bottom: 0.3125rem;
-  margin-right: .01rem;
-  font-size: 1.25rem;
-  text-decoration: none;
-  white-space: nowrap;
-`;
 const Main = styled.div`
   display: block;
 `;
@@ -452,17 +414,7 @@ const Tabs = styled.div`
     flex: 1;
   }
 `;
-const Content = styled.div`
-  background-color: #f7f7f7;
-  padding: 2em;
-  border-radius: 5px;
-`;
 
-const Title = styled.h1`
-  font-size: 1.5em;
-  text-align: center;
-  color: palevioletred;
-`;
 
 const TabsButton = styled.button`
   font-weight: 600;
@@ -489,171 +441,7 @@ const TabsButton = styled.button`
     background: #0091ff;
   }
 `;
-const H2 = styled.h2`
-  font-size: 19px;
-  line-height: 22px;
-  color: #11181c;
-  margin: 0 0 8px;
-`;
-const Card = styled.div`
-  border-radius: 12px;
-  background: #fff;
-  border: ${(div) => (div.selected ? "1px solid black" : "1px solid #eceef0")};
-  box-shadow: 0px 1px 3px rgba(16, 24, 40, 0.1),
-    0px 1px 2px rgba(16, 24, 40, 0.06);
-`;
 
-const CardBody = styled.div`
-  padding: 16px;
-  display: flex;
-  gap: 16px;
-  align-items: center;
-
-  > * {
-    min-width: 0;
-  }
-`;
-
-const CardFooter = styled.div`
-  display: flex;
-  justify-content: space-around;
-  flex-wrap: wrap;
-  gap: 4px;
-  padding: 16px;
-  border-top: 1px solid #eceef0;
-`;
-
-const TextLink = styled.a`
-  display: block;
-  margin: 0;
-  font-size: 14px;
-  line-height: 20px;
-  color: ${(p) => (p.bold ? "#11181C !important" : "#687076 !important")};
-  font-weight: ${(p) => (p.bold ? "600" : "400")};
-  font-size: ${(p) => (p.small ? "12px" : "14px")};
-  overflow: ${(p) => (p.ellipsis ? "hidden" : "visible")};
-  text-overflow: ${(p) => (p.ellipsis ? "ellipsis" : "unset")};
-  white-space: nowrap;
-  outline: none;
-
-  &:focus,
-  &:hover {
-    text-decoration: underline;
-  }
-`;
-
-const Thumbnail = styled.a`
-  display: block;
-  width: 48px;
-  height: 48px;
-  flex-shrink: 0;
-  border: 1px solid #eceef0;
-  border-radius: 8px;
-  overflow: hidden;
-  outline: none;
-  transition: border-color 200ms;
-
-  &:focus,
-  &:hover {
-    border-color: #d0d5dd;
-  }
-
-  img {
-    object-fit: cover;
-    width: 100%;
-    height: 100%;
-  }
-`;
-
-const CardWrapper = styled.div`
-  margin: 0 0 16px;
-`;
-
-const sharedButtonStyles = `
-  display: inline-flex;
-  align-items: center;
-  gap: 8px;
-  padding: 8px 16px;
-  margin-bottom: 12px;
-  height: 32px;
-  border-radius: 6px;
-  font-weight: 600;
-  font-size: 12px;
-  line-height: 15px;
-  text-align: center;
-  cursor: pointer;
-
-  &:hover,
-  &:focus {
-    text-decoration: none;
-    outline: none;
-  }
-
-  i {
-    color: #7E868C;
-  }
-
-  .bi-16 {
-    font-size: 16px;
-  }
-`;
-
-const Button = styled.button`
-  ${sharedButtonStyles}
-  color: ${(p) => (p.primary ? "#fff" : "#11181C")} !important;
-  background: ${(p) => (p.primary ? "#0091FF" : "#FBFCFD")};
-  border: ${(p) => (p.primary ? "none" : "1px solid #D7DBDF")};
-
-  &:hover,
-  &:focus {
-    background: ${(p) => (p.primary ? "#0484e5" : "#ECEDEE")};
-  }
-`;
-
-const ButtonLink = styled.a`
-  ${sharedButtonStyles}
-  color: ${(p) => {
-    if (p.primary) return "#fff";
-    else if (p.danger) return "#fff";
-    else return "#11181C";
-  }} !important;
-  background: ${(p) => {
-    if (p.primary) return "#0091FF";
-    else if (p.danger) return "#dc3545";
-    else return "#FBFCFD";
-  }};
-  border: ${(p) => (p.primary ? "none" : "1px solid #D7DBDF")};
-
-  &:hover,
-  &:focus {
-    background: ${(p) => {
-    if (p.primary) return "#0484e5";
-    else if (p.danger) return "#b22b38";
-    else return "#ECEDEE";
-  }}
-`;
-
-const SignUpLink = styled.a`
-  --blue: RGBA(13, 110, 253, 1);
-  display: ${({ hidden }) => (hidden ? "none" : "inline-block")};
-  font-size: 14px;
-  cursor: pointer;
-  color: var(--blue);
-  text-decoration: none;
-  margin-left: 0.1em;
-  padding: 0;
-  white-space: nowrap;
-
-  &:hover {
-    color: var(--blue);
-    text-decoration: none;
-  }
-
-  &:visited {
-    color: var(--blue);
-    text-decoration: none;
-  }
-`;
 
 const selectTab = (tabName) => {
   Storage.privateSet("queryapi:activeTab", tabName);
@@ -665,58 +453,9 @@ const selectIndexerPage = (viewName) => {
   setActiveIndexerTabView(viewName);
 };
 
-const indexerView = (accountId, indexerName) => {
-  const editUrl = `https://dev.near.org/${REPL_ACCOUNT_ID}/widget/QueryApi.App?selectedIndexerPath=${accountId}/${indexerName}`;
-  const statusUrl = `https://dev.near.org/${REPL_ACCOUNT_ID}/widget/QueryApi.App?selectedIndexerPath=${accountId}/${indexerName}&view=indexer&activeIndexerView=status`;
-  const playgroundLink = `https://cloud.hasura.io/public/graphiql?endpoint=${REPL_GRAPHQL_ENDPOINT}/v1/graphql&header=x-hasura-role%3A${accountId.replaceAll(
-    ".",
-    "_"
-  )}`;
-
-  return (
-    <Card>
-      <CardBody>
-        <Thumbnail>
-          <Widget
-            src="mob.near/widget/Image"
-            props={{
-              image: metadata.image,
-              fallbackUrl:
-                "https://upload.wikimedia.org/wikipedia/commons/8/86/Database-icon.svg",
-              alt: "Near QueryApi indexer",
-            }}
-          />
-        </Thumbnail>
-
-        <div>
-          <TextLink as="a" bold ellipsis>
-            {indexerName}
-          </TextLink>
-          <TextLink as="a" ellipsis>
-            @{accountId}
-          </TextLink>
-        </div>
-      </CardBody>
-
-      <CardFooter className="flex justify-center items-center">
-        <ButtonLink onClick={() => selectIndexerPage("status")}>
-          View Status
-        </ButtonLink>
-        <ButtonLink primary onClick={() => selectIndexerPage("editor")}>
-          {accountId === context.accountId ? "Edit Indexer" : "View Indexer"}
-        </ButtonLink>
-        <ButtonLink href={playgroundLink} target="_blank">
-          View In Playground
-        </ButtonLink>
-      </CardFooter>
-    </Card>
-  );
-};
-
-
 return (
   <Wrapper negativeMargin={activeTab === "explore"}>
-    <Tabs>
+    {/* <Tabs>
       <TabsButton
         type="button"
         onClick={() => selectTab("explore")}
@@ -745,7 +484,7 @@ return (
           </TabsButton>
         </>
       )}
-    </Tabs>
+    </Tabs> */}
 
 
     <Main>
@@ -770,16 +509,22 @@ return (
                       <CheckboxContainer key={index}>
                         <CheckboxLabel>
                           <Checkbox
+                            type="checkbox"
+                            id={item.method_name}
+                            checked={checkboxState[item.method_name]}
                             onChange={() => handleParentChange(item.method_name)}
-                            type="checkbox" id={item.method_name} />
+                          />
                           {item.method_name}
                         </CheckboxLabel>
                         {item.schema.properties && (
                           <SubCheckboxContainer>
                             {Object.keys(item.schema.properties).map((property, subIndex) => (
                               <CheckboxLabel key={subIndex}>
-                                <Checkbox type="checkbox" id={`${item.method_name}_${property}`}
-                                  onChange={() => handleChildChange(item.method_name, property)}
+                                <Checkbox
+                                  type="checkbox"
+                                  id={`${item.method_name}::${property}`}
+                                  checked={checkboxState[`${item.method_name}::${property}`]}
+                                  onChange={() => handleChildChange(`${item.method_name}::${property}`)}
                                 />
                                 {property}: {item.schema.properties[property].type}
                               </CheckboxLabel>
@@ -808,124 +553,6 @@ return (
         </ExploreIndexersContainer>
 
 
-        {/* <NavBarLogo
-          href={`https://dev.near.org/${REPL_ACCOUNT_ID}/widget/QueryApi.App`}
-          title="QueryApi"
-          onClick={() => selectTab("explore")}
-        >
-          <Widget
-            src="mob.near/widget/Image"
-            props={{
-              className: "d-inline-block align-text-top me-2",
-              image: metadata.image,
-              style: { height: "24px" },
-              fallbackUrl:
-                "https://upload.wikimedia.org/wikipedia/commons/8/86/Database-icon.svg",
-              alt: "the queryapi logo",
-            }}
-          />
-          QueryApi
-        </NavBarLogo>
-
-        <SignUpLink target="_blank" href={`https://docs.near.org/build/data-infrastructure/query-api/intro`}>
-          (Documentation)
-        </SignUpLink>
-        <div>
-          <ButtonLink
-            href={`/${REPL_ACCOUNT_ID}/widget/QueryApi.App/?view=create-new-indexer`}
-            style={{ "margin-top": "10px" }}
-            onClick={() => {
-              setSelectedIndexerName("");
-              State.update({
-                activeTab: "create-new-indexer",
-              });
-              selectTab("create-new-indexer");
-            }}
-          >
-            Create New Indexer
-          </ButtonLink>
-          {myIndexers.length > 0 && (
-            <H2>
-              {accountId}'s Indexers
-              <span>({myIndexers.length})</span>
-            </H2>
-          )}
-          <Widget
-            src={`${REPL_ACCOUNT_ID}/widget/QueryApi.IndexerExplorer`}
-          />
-        </div> */}
-      </Section>
-
-
-
-
-
-
-
-
-
-
-
-
-
-      <Section
-        negativeMargin
-        primary
-        active={activeTab === "create-new-indexer"}
-      >
-        {activeTab === "create-new-indexer" && (
-          <div>
-            <Widget
-              src={`${REPL_ACCOUNT_ID}/widget/QueryApi.Editor`}
-              props={{
-                indexerName:
-                  selected_indexerName ?? state.indexers[0].indexerName,
-                accountId: selected_accountId ?? state.indexers[0].accountId,
-                path: "create-new-indexer",
-              }}
-            />
-          </div>
-        )}
-      </Section>
-      <Section negativeMargin primary active={activeTab === "indexer"}>
-        <Editor>
-          {state.indexers.length > 0 &&
-            (selectedIndexerName ? (
-              <H2>{selectedIndexerName}</H2>
-            ) : (
-              <H2>{`${state.indexers[0].accountId}/${state.indexers[0].indexerName}`}</H2>
-            ))}
-          <Widget
-            src={`${REPL_ACCOUNT_ID}/widget/QueryApi.Editor`}
-            props={{
-              indexerName:
-                selected_indexerName ?? state.indexers[0].indexerName,
-              accountId: selected_accountId ?? state.indexers[0].accountId,
-              path: "query-api-editor",
-              tab: props.tab,
-              activeView: activeIndexerView
-            }}
-          />
-        </Editor>
-        {activeTab === "create-new-indexer" && (
-          <div>
-            {state.indexers.length > 0 &&
-              (selectedIndexerName ? (
-                <H2>{selectedIndexerName}</H2>
-              ) : (
-                <H2>{`${state.indexers[0].accountId}/${state.indexers[0].indexerName}`}</H2>
-              ))}
-            <Widget
-              src={`${REPL_ACCOUNT_ID}/widget/QueryApi.Editor`}
-              props={{
-                indexerName:
-                  selected_indexerName ?? state.indexers[0].indexerName,
-                accountId: selected_accountId ?? state.indexers[0].accountId,
-                path: "create-new-indexer",
-              }}
-            />
-          </div>
-        )}
       </Section>
     </Main>
   </Wrapper>
