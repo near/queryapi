509679eab278e0e78c478a92738374c3834476e7
Morgan Mccauley <morgan@mccauley.co.nz>
2024-07-18 16:36:13 +1200

    fix: Dont report `Stalled` for streams which have just started

--- coordinator/src/handlers/block_streams.rs
+++ coordinator/src/handlers/block_streams.rs
@@ -246,7 +246,7 @@ impl BlockStreamsHandler {
             let updated_at =
                 SystemTime::UNIX_EPOCH + Duration::from_secs(health.updated_at_timestamp_secs);
 
-            let stale = updated_at.elapsed().unwrap_or_default() > Duration::from_secs(30);
+            let stale = updated_at.elapsed().unwrap_or_default() > Duration::from_secs(60);
             let stalled = matches!(
                 health.processing_state.try_into(),
                 Ok(ProcessingState::Stalled)
