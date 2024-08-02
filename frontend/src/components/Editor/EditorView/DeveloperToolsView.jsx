67f5983a186f503cd67af17ec9d016fe97a22b69
kevin <kevin101zhang@gmail.com>
2024-08-02 17:00:33 -0400

    chore: renamed and removed jsx

--- frontend/src/components/Editor/EditorView/DeveloperToolsView.jsx
+++ frontend/src/components/Editor/EditorView/DeveloperToolsView.jsx
@@ -26,9 +26,6 @@ const DeveloperToolsView = ({
 
   return (
     <div className="bg-gray-100 rounded-lg p-2 mb-0 mx-2">
-      <div className="text-center mb-3">
-        <span className="text-xs font-medium">Developer Tools</span>
-      </div>
       <div className="flex justify-between">
         <div className="flex space-x-1">
           <button
@@ -59,9 +56,8 @@ const DeveloperToolsView = ({
           <div className="h-full border-r border-gray-300 mx-4"></div>
 
           <div
-            className={`relative flex items-center justify-center px-2 py-1 bg-gray-200 hover:bg-gray-300 text-gray-700 text-xs rounded ${
-              diffView ? 'bg-gray-600 hover:bg-gray-700 text-white' : 'bg-gray-200 hover:bg-gray-300'
-            }`}
+            className={`relative flex items-center justify-center px-2 py-1 bg-gray-200 hover:bg-gray-300 text-gray-700 text-xs rounded ${diffView ? 'bg-gray-600 hover:bg-gray-700 text-white' : 'bg-gray-200 hover:bg-gray-300'
+              }`}
             onClick={handleCodeGen}
           >
             <span className="px-3">Diff View</span>
@@ -77,9 +73,8 @@ const DeveloperToolsView = ({
 
           <CustomTooltip message="Please Open Browser Console" direction={TooltipDirection.Top}>
             <div
-              className={`relative flex items-center justify-center px-2 py-1 bg-gray-200 hover:bg-gray-300 text-gray-700 text-xs rounded ${
-                debugMode ? 'bg-gray-600 hover:bg-gray-700 text-white' : 'bg-gray-200 hover:bg-gray-300'
-              }`}
+              className={`relative flex items-center justify-center px-2 py-1 bg-gray-200 hover:bg-gray-300 text-gray-700 text-xs rounded ${debugMode ? 'bg-gray-600 hover:bg-gray-700 text-white' : 'bg-gray-200 hover:bg-gray-300'
+                }`}
               onClick={handleCodeGen}
             >
               <span className="px-3">Debug Mode</span>
--- frontend/src/components/Editor/EditorView/EditorMenuView.jsx
+++ frontend/src/components/Editor/EditorView/EditorMenuView.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Navbar, Container, InputGroup, Form, ButtonGroup, Button } from 'react-bootstrap';
+import { Navbar, Container, InputGroup, ButtonGroup, Button } from 'react-bootstrap';
 import { Braces, ArrowCounterclockwise, FileText, TrashFill } from 'react-bootstrap-icons';
 import CustomTooltip, { TooltipDirection } from '@/components/Common/CustomTooltip';
 
@@ -21,19 +21,15 @@ const EditorMenuView = ({
     <Navbar bg="white" variant="light" className="shadow-sm p-3 bg-white rounded">
       <Container fluid className="d-flex flex-wrap justify-content-between align-items-center">
         <div className="d-flex flex-wrap align-items-center">
-          <span className="me-4 font-weight-bold text-secondary text-sm">
-            Indexer: {accountId}/{indexerName ? indexerName : 'Null'}
-          </span>
-          <span className="me-4 font-weight-bold text-secondary text-sm">
-            Filter:{' '}
-            {!isCreateNewIndexer ? (
-              indexerDetails.rule.affected_account_id
-            ) : (
-              <InputGroup size="sm" hasValidation={true} style={{ width: 'fit-content' }}>
-                <Form.Control.Feedback type="invalid">Please provide a valid contract name.</Form.Control.Feedback>
-              </InputGroup>
-            )}
-          </span>
+          {accountId && indexerName && (
+            <span className="me-4 font-weight-bold text-secondary text-sm">
+              Indexer: {accountId}/{indexerName}
+            </span>)}
+
+          {!isCreateNewIndexer && (
+            <span className="me-4 font-weight-bold text-secondary text-sm">
+              Filter: {indexerDetails.rule.affected_account_id}
+            </span>)}
         </div>
         <ButtonGroup className="mt-3 mt-md-0">
           {isUserIndexer && !isCreateNewIndexer && (
