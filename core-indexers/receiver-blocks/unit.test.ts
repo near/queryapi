f190d5602bd388f8d115baed7029e84463ef4d05
Darun Seethammagari <darunrs@gmail.com>
2024-08-07 17:24:10 -0700

    Add simple unit test for bitmap indexer

--- core-indexers/receiver-blocks/unit.test.ts
+++ core-indexers/receiver-blocks/unit.test.ts
@@ -17,7 +17,42 @@ describe('Receiver Blocks Indexer Tests', () => {
     const localIndexer = new LocalIndexer(indexerConfig);
     const context = localIndexer.getContext();
 
+    // Run on one block to populate receivers table and initial bitmap
     await localIndexer.executeOnBlock(100000000);
-    const receivers = context.db.Receivers.select({})
+    const receivers = await context.db.Receivers.select({
+      receiver: 'app.nearcrowd.near'
+    });
+    const tokenSweatId = receivers[0].id;
+
+    const correctBitmapOne = {
+      first_block_height: 100000000,
+      block_date: '2023-08-30',
+      receiver_id: tokenSweatId,
+      bitmap: 'wA==',
+      last_elias_gamma_start_bit: 1,
+      max_index: 0,
+    };
+    const correctBitmapTwo = {
+      first_block_height: 100000000,
+      block_date: '2023-08-30',
+      receiver_id: tokenSweatId,
+      bitmap: 'oA==',
+      last_elias_gamma_start_bit: 1,
+      max_index: 1,
+    };
+
+    let bitmap = await context.db.Bitmaps.select({
+      receiver_id: tokenSweatId
+    });
+    expect(bitmap.length).toBe(1);
+    expect(bitmap[0]).toEqual(correctBitmapOne);
+
+    // Run on second block and verify bitmap update
+    await localIndexer.executeOnBlock(100000001);
+    bitmap = await context.db.Bitmaps.select({
+      receiver_id: tokenSweatId
+    });
+    expect(bitmap.length).toBe(1);
+    expect(bitmap[0]).toEqual(correctBitmapTwo);
   });
 });
