e5e93eb3ea14ef2bdc1bef6c488c2841d98b405f
Darun Seethammagari <darunrs@gmail.com>
2024-08-13 22:07:54 -0700

    Remove data layer check before call

--- runner-client/examples/start_provisioning_task.rs
+++ runner-client/examples/start_provisioning_task.rs
@@ -1,16 +1,17 @@
 use tonic::Request;
 
 use runner::data_layer::data_layer_client::DataLayerClient;
-use runner::data_layer::DeprovisionRequest;
+use runner::data_layer::ProvisionRequest;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut client = DataLayerClient::connect("http://localhost:7001").await?;
 
     let response = client
-        .start_deprovisioning_task(Request::new(DeprovisionRequest {
-            account_id: "bucanero.near".to_string(),
-            function_name: "nft_v4".to_string(),
+        .start_provisioning_task(Request::new(ProvisionRequest {
+            account_id: "test.near".to_string(),
+            function_name: "data_layer_example".to_string(),
+            schema: "create table blocks();".to_string(),
         }))
         .await?;
 
